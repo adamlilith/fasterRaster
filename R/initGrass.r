4054251ad6e453a828177e2090349976d71d9296
Adam B. Smith <adam@earthskysea.org>
2020-09-03 10:13:27 -0500

    User can name vector/raster in GRASS session

--- R/initGrass.r
+++ R/initGrass.r
@@ -4,6 +4,8 @@
 #' @param alreadyInGrass Logical, if \code{FALSE} (default) then start a new GRASS session and import the raster named in \code{rast} or the vector named in \code{vect}. If \code{FALSE}, use a raster or vector already in GRASS with the name given by \code{rast} or \code{vect}. The latter case is useful if you are chaining \pkg{fasterRaster} functions together and the first function initializes the session. The first function should use \code{alreadyInGrass = FALSE} and subsequent functions should use \code{alreadyInGrass = TRUE} then use their \code{rast} (or \code{vect}) arguments to name the raster (or vector) that was made by the previous function.
 #' @param rast Either: a raster object \emph{or} the name of a raster already imported into GRASS \emph{or} \code{NULL} (default) in which case no raster is exported into GRASS. Either \code{rast} or \code{vect} (or both) must be non-\code{NULL}.
 #' @param vect Either: a SpatialPoints, SpatialPolygons, or SpatialLines object \emph{or} the name of a vector dataset already imported into GRASS \emph{or} \code{NULL} (default) in which case no vector is exported into GRASS. Either \code{rast} or \code{vect} (or both) must be non-\code{NULL}.
+#' @param rastName Character. Name of the raster when exported to GRASS. Default is \code{'rast'}.
+#' @param vectName Character. Name of the vector when exported to GRASS. Default is \code{'vect'}.
 #' @param mapset Character. Mapset of GRASS location. The default name is \code{'PERMANENT'}. Typically, it is not a good idea to change this, as most functions in the \pkg{fasterRaster} package assume the mapset is named "default".
 #' @param location Character. Name of GRASS location. The default name is \code{'default'}. Typically, it is not a good idea to change this, as most functions in the \pkg{fasterRaster} package assume the location is named "default".
 #' @param tempDir Character. Path name of directory in which to create the GRASS mapset. The default is to create a directory in the location for temporary files for the operating system.
@@ -24,6 +26,8 @@ initGrass <- function(
 	alreadyInGrass = FALSE,
 	rast = NULL,
 	vect = NULL,
+	rastName = 'rast',
+	vectName = 'vect',
 	mapset = 'PERMANENT',
 	location = 'default',
 	tempDir = tempfile(),
@@ -89,18 +93,18 @@ initGrass <- function(
 		# export raster
 		if (!is.null(rast)) {
 	
-			input <- c(input, 'rast')
+			input <- c(input, rastName)
 			names(input)[length(input)] <- 'rastNameInGrass'
-			exportRastToGrass(rast, vname='rast', tempDir=tempDir)
+			exportRastToGrass(rast, vname=rastName, tempDir=tempDir)
 			
 		}
 		
 		# export vector
 		if (!is.null(vect)) {
 		
-			input <- c(input, 'vect')
+			input <- c(input, vectName)
 			names(input)[length(input)] <- 'vectNameInGrass'
-			exportVectToGrass(vect, vname='vect')
+			exportVectToGrass(vect, vname=vectName)
 
 		}
 		
