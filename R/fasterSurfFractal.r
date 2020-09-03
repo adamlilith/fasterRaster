578f070247494a515922dc2b7a6d5567f0af766f
Adam B. Smith <adam@earthskysea.org>
2020-09-02 21:23:44 -0500

    Create fasterSurfFractal.r

---
+++ R/fasterSurfFractal.r
@@ -0,0 +1,52 @@
+#' Generate a raster with a fractal pattern
+#'
+#' This function creates a raster with a fractal pattern. It utilizes the GRASS function \code{r.surf.fractal}.
+#' @param rast Either a raster or the name of a raster in an existing GRASS session to serve as a template.
+#' @param dimension Numeric. Fractal dimension. Must be >2 and <3. Default is 2.05.
+#' @param alreadyInGrass Logical, if \code{FALSE} (default) then start a new GRASS session and import the raster named in \code{rast}. If \code{FALSE}, use a raster already in GRASS with the name given by \code{rast}. The latter is useful if you are chaining \pkg{fasterRaster} functions together and the first function initializes the session. The first function should use \code{alreadyInGrass = FALSE} and subsequent functions should use \code{alreadyInGrass = TRUE} then use their \code{rast} (or \code{vect}) arguments to name the raster (or vector) that was made by the previous function.
+#' @param grassToR Logical, if \code{TRUE} (default) then the product of the calculations will be returned to R. If \code{FALSE}, then the product is left in the GRASS session and named \code{longitude} and \code{latitude}. The latter case is useful (and faster) when chaining several \pkg{fasterRaster} functions together.
+#' @param ... Arguments to pass to \code{\link[rgrass7]{execGRASS}} when calculating horizon height (i.e., function \code{r.horizon} in GRASS).
+#' @return If \code{grassToR} if \code{TRUE}, then a raster object with the same coordinate reference system, resolution, and extent as \code{rast}. Otherwise, a vector is written into the GRASS session. The name of this vector is as \code{surfFractal}.
+#' @details See (r.surf.fractal)[https://grass.osgeo.org/grass78/manuals/r.surf.fractal.html] for more details. Note that if you get an error saying "", then you should add the EPSG code to the beginning of the raster coordinate reference system string (its "proj4string"). For example, \code{proj4string(rast) <- CRS('+init=epsg:32738')}. EPSG codes for various projections, datums, and locales can be found at (Spatial Reference)[http://spatialreference.org/].
+#' @seealso
+#' @examples
+#' \donttest{
+#' # change this to where GRASS 7 is installed on your system
+#' grassDir <- 'C:/Program Files/GRASS GIS 7.8'
+#'
+#' data(madElev)
+#' fract1 <- fasterSurfFractal(rast=madElev, dimension=2.1, grassDir=grassDir)
+#' fract2 <- fasterSurfFractal(rast='rast', dimension=2.9,
+#' alreadyInGrass=TRUE, grassDir=grassDir)
+#' plot(stack(fract1, fract2))
+#' }
+#' @export
+
+fasterSurfFractal <- function(
+	rast,
+	dimension = 2.05,
+	grassDir = NULL,
+	alreadyInGrass = FALSE,
+	grassToR = TRUE,
+	...
+) {
+
+	flags <- c('quiet', 'overwrite')
+	
+	if (dimension <= 2 | dimension >= 3) stop('Argument "dimension" must be >2 and <3.')
+	
+	# initialize GRASS
+	input <- initGrass(alreadyInGrass, rast=rast, vect=NULL, grassDir=grassDir)
+	
+	rgrass7::execGRASS('r.surf.fractal', dimension=dimension, output='surfFractal', flags=flags, ...)
+
+	if (grassToR) {
+
+		out <- rgrass7::readRAST('surfFractal')
+		out <- raster::raster(out)
+		names(out) <- 'surfFractal'
+		out
+		
+	}
+	
+}
