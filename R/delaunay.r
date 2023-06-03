6d00f5c1e8a724e5babd6f3492bc2e081c58b56f
Adam B. Smith <adam@earthskysea.org>
2023-06-03 02:21:11 -0500

    Create delaunay.r

---
+++ R/delaunay.r
@@ -0,0 +1,36 @@
+#' Delaunay triangulation for points
+#'
+#' @description This function creates a Delaunay triangulation from a set of spatial points.
+#'
+#' @param x A `GVector` "points" object.
+#'
+#' @returns A `GVector`.
+#'
+#' @seealso [terra::delaunay()], module `v.delaunay` in **GRASS**
+#'
+#' @example man/examples/ex_pointOperations.r
+#'
+#' @aliases delaunay
+#' @rdname delaunay
+#' @exportMethods delaunay
+methods::setMethod(
+	f = 'delaunay',
+	signature = c(x = 'GVector'),
+	definition = function(x) {
+
+	if (geomtype(x) != 'points') stop('The vector must represent spatial points.')
+	
+	gn <- .makeGname('delaunay', 'vect')
+	args <- list(
+		cmd = 'v.delaunay',
+		input = gnames(x),
+		output = gn,
+		flags = c('quiet', 'overwrite'),
+		intern = TRUE
+	)
+
+	do.call(rgrass::execGRASS, args=args)
+	makeGVector(gn)
+	
+	} # EOF
+)
