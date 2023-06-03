6ff0fcfdd8b4ab7b659e867b183fb3fb878154c4
Adam B. Smith <adam@earthskysea.org>
2023-06-03 02:21:28 -0500

    Make zres()

--- R/res.r
+++ R/res.r
@@ -1,28 +1,42 @@
-#' Spatial resolution of a 'GRaster'
+#' Spatial resolution
 #'
-#' @description Spatial resolution of a `GRaster`. `res()` reports 2-dimensional (x and y) resolution. `res3d()` reports resolution for 3-dimensional rasters.
+#' @description Spatial resolution of a `GRaster`:
+#' 
+#' `res()`: 2-dimensional resolution (x and y) .\cr
+#' `res3d()`: 3-dimensinal resolution (z, y, and z).\cr
+#' `zres()`: Resolution of the third dimension (z).
 #'
 #' @param x A `GRaster`.
 #'
-#' @return A numeric vector. For both `res()` and `res3d()`, the first value is the length of cells in the x-direction and the second the length of cells in the y-direction. For `res3d()` the third value is height of a voxel (the z-direction).
+#' @return A numeric vector. For both `res()` and `res3d()`, the first value is the length of cells in the x-direction and the second the length of cells in the y-direction. For `res3d()` the third value is height of a voxel (the z-direction). For `zres()` the value is the resolution in the z-direction.
+#'
+#' @seealso [terra::res()]
 #'
 #' @example man/examples/ex_GRaster.r
 #'
 #' @aliases res
 #' @rdname res
-#' @export
-#' @exportMethod res3d
+#' @exportMethod res
 setMethod(
 	f = 'res',
-	signature = 'GRaster',
+	signature = 'GRegion',
 	definition = function(x) x@resolution[1L:2L]
 )
 
-#' @rdname res
 #' @aliases res3d
+#' @rdname res
 #' @exportMethod res3d
 setMethod(
 	f = 'res3d',
-	signature = 'GRaster',
-	definition = function(x) x@resolution
+	signature = 'GRegion',
+	definition = function(x) c(xres=x@resolution[1L], yres=x@resolution[2L], zres=x@resolution[3L])
+)
+
+#' @aliases zres
+#' @rdname res
+#' @exportMethod zres
+setMethod(
+	f = 'zres',
+	signature = 'GRegion',
+	definition = function(x) x@resolution[3L]
 )
