3d5dedcc5a5afe59bda92b92123ba7a489bbbc1c
Adam B. Smith <adam@earthskysea.org>
2023-07-21 00:27:35 -0500

    Add message on GRASS version

--- R/aaa.r
+++ R/aaa.r
@@ -17,5 +17,8 @@
 	# packageStartupMessage('It is recommended to attach the terra and sf packages before')
 	# packageStartupMessage('fasterRaster using, for example, "library(terra)".')
 	packageStartupMessage('To use fasterRaster functions, please connect to GRASS using faster().')
+	packageStartupMessage('Functions will be executed assuming you are using GRASS ', .grassVerDefault(), '.')
+	packageStartupMessage('You can set the version of GRASS you are using with the function setFastOptions().')
+
 	
 }
