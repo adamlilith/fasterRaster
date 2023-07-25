b29f77a7aa46e856cc6a6f5bad38af3ed8c391e6
Adam B. Smith <adam@earthskysea.org>
2023-07-24 23:44:22 -0500

    Create as.points.r

---
+++ R/as.points.r
@@ -0,0 +1,95 @@
+#' Convert a lines or polygons vector to points
+#'
+#' @description 'as.points()' converts a `GRaster` or `GVector` to a points `GVector`. For rasters, the points have the coordinates of cell centers and ba assigned the cell's value in the attribute table. One non-`NA` cells will be converted to a point. For vectors, each point will have the attributes of the line or polygon to which it belonged. Points are extracted from each vertex.
+#' 
+#' @param x A `GRaster`, `GVector`.
+#' 
+#' @param values Logical: If `TRUE` (default), create an attribute table with raster cell values, with one row per point.
+#' 
+#' @returns A `points` `GVector`.
+#' 
+#' @seealso [terra::as.points()] and &** GRASS** modules `v.to.points` and `r.to.vect`
+#' 
+#' @example man/examples/ex_asPoints.r
+#' 
+#' @aliases as.points
+#' @rdname as.points
+#' @setMethod as.points
+methods::setMethod(
+    f = "as.points",
+    signature = c(x = 'GVector'),
+    function(x) {
+
+    if (geomtype(x) == "points") {
+        warning("Vector object is already a points object.")
+        return(x)
+    }
+
+    .restore(x)
+
+    gtype <- geomtype(x)
+    if (gtype == "points") {
+        warning("Vector object is already a points object.")
+        return(x)
+    } else if (gtype == "lines") {
+        type <- "line"
+    } else if (gtype == "polygons") {
+        type <- "area"
+        # type <- "boundary"
+    }
+
+    gn <- .makeGName(NULL, "vector")
+    args <- list(
+        cmd = "v.to.points",
+        input = .gnames(x),
+        output = gn,
+        use = "vertex",
+        type = type,
+        flags = c("quiet", "overwrite"),
+        intern = TRUE
+    )
+    do.call(rgrass::execGRASS, args = args)
+    .makeGVector(gn)
+
+    } # E#OF
+)
+
+#' @aliases as.points
+#' @rdname as.points
+#' @setMethod as.points
+methods::setMethod(
+    f = "as.points",
+    signature = c(x = "GRaster"),
+    function(x, values = TRUE) {
+
+    .restore(x)
+    region(x)
+
+    if (nlyr(x) > 1L) warning("The raster has more than one layer. Only the first layer will be converted to points.")
+
+    gn <- .makeGName(NULL, "vector")
+
+    args <- list(
+        cmd = "r.to.vect",
+        input = .gnames(x)[1L],
+        output = gn,
+        type = "point",
+        column = names(x)[1L],
+        flags = c("quiet", "overwrite"),
+        intern = TRUE
+    )
+    if (!values) args$flags <- c(args$flags, "t")
+    do.call(rgrass::execGRASS, args = args)
+
+    # drop "label" column
+    info <- rgrass::execGRASS("v.info", map = gn, flags = c("c", "quiet"), intern = TRUE)
+    if (length(info) == 3L & info[3L] == 'CHARACTER|label') {
+
+        rgrass::execGRASS("v.db.dropcolumn", map = gn, columns = "label", flags = "verbose", intern = TRUE)
+
+    }
+
+    .makeGVector(gn)
+
+    } # EOF
+)
