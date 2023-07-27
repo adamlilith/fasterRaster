773ac4ee0791629c039a54006976c6d0c5393995
Adam B. Smith <adam@earthskysea.org>
2023-07-26 22:21:28 -0500

    Works for all cases

--- R/crop.r
+++ R/crop.r
@@ -1,9 +1,15 @@
 #' Remove parts of a GRaster or GVector
 #'
-#' `crop()` removes parts of a `GRaster` or `GVector` that fall "outside" another raster or vector.
+#' `crop()` removes parts of a `GRaster` or `GVector` that fall "outside" another raster or vector. You cannot make the `GRaster` or `GVector` larger than it already is (see [extend()]). Rasters may not be cropped to the exact extent, as the extent will be enlarged to encompass an integer number of cells. If you wish to remove certain cells of a raster, see [mask()].
 #'
 #' @param x A `GRaster` or `GVector` to be cropped.
 #' @param y A `GRaster` or `GVector` to serve as a template for cropping.
+#' @param ext Logical (when `x` and `y` are `GVector`s): If `TRUE`, use the extent of `y` to crop `x`. Otherwise:
+#' * If `ext` is `TRUE, and `x` and `y` are "points" `GVector`s, use the convex hull around `y` to select points in `x`.
+#' 
+#' @details Known differences from [terra::crop()]:
+#' * If `x` and `y` are "points" vectors, and `ext` is `TRUE`, **terra** removes points that fall on the extent boundary. **fasterRaster** does not remove points on the extent boundary.
+#' * If `x` is a "points" vector and `y` is a "lines" vectors, and `ext` is `FALSE`, **terra** uses the extent of `y` to crop the points.  **fasterRaster** uses the minimum convex hull of the lines vector.
 #'
 #' @return A `GRaster` or `GVector`.
 #' 
@@ -19,31 +25,80 @@ methods::setMethod(
 	signature = c(x = "GRaster"),
 	definition = function(x, y) {
 	
-	compareGeom(x, y)
+	if (inherits(y, "GVector")) {
+		compareGeom(x, y)
+	} else {
+		if (crs(x) != crs(y)) stop("Rasters have different coordinate references systems.")
+	}
 	.restore(x)
 
-	### change region to match the extent of y but have the same resolution as x
-	region(x)
+	### change region to match the extent of y but have the same resolution and registration as x
+	if (west(y) <= west(x)) {
+		w <- west(y)
+	} else {
+  		cells <- (west(y) - west(x)) / xres(x)
+  		cells <- ceiling(cells)
+  		w <- west(x) + cells * xres(x)
+	}
+
+	if (east(y) >= east(x)) {
+		e <- east(x)
+	} else {
+  		cells <- (east(x) - east(y)) / xres(x)
+		if (inherits(y, "GVector")) {
+			cells <- floor(cells)
+		} else if (inherits(y, "GRaster")) {
+			cells <- floor(cells) # !!!
+		}
+  		e <- east(x) - cells * xres(x)
+	}
+
+	if (south(y) <= south(x)) {
+		s <- south(x)
+	} else {
+  		cells <- (south(y) - south(x)) / yres(x)
+  		if (inherits(y, "GVector")) {
+			cells <- floor(cells)
+		} else if (inherits(y, "GRaster")) {
+   			cells <- ceiling(cells)
+		}
+  		s <- south(x) + cells * yres(x)
+	}
+
+	if (north(y) >= north(x)) {
+		n <- north(x)
+	} else {
+  		cells <- (north(x) - north(y)) / yres(x)
+		if (inherits(y, "GVector")) {
+			cells <- floor(cells)
+		} else if (inherits(y, "GRaster")) {
+			cells <- ceiling(cells)
+		}
+  		n <- north(x) - cells * yres(x)
+	}
+
+	w <- as.character(w)
+	e <- as.character(e)
+	s <- as.character(s)
+	n <- as.character(n)
+	ewres <- as.character(xres(x))
+	nsres <- as.character(yres(x))
 
 	args <- list(
 		cmd = "g.region",
-		flags = c("quiet", "overwrite", "o"),
+		w = w, e = e, s = s, n = n,
+		ewres = ewres, nsres = nsres,
+		flags = c("quiet", "overwrite"),
+		# flags = c("quiet", "overwrite", "o"),
 		intern = TRUE
 	)
 
-	if (inherits(y, "GRaster")) {
-		args <- c(args, list(raster = .gnames(y)))
-	} else if (inherits(y, "GVector")) {
-		args <- c(args, list(vector = .gnames(y)))
-	}
-	
-	do.call(rgrass::execGRASS, args=args)
+ 	do.call(rgrass::execGRASS, args = args)
 
 	### crop by creating copy of focal raster
 	out <- list()
 	gns <- .copyGSpatial(x, reshapeRegion = FALSE)
 	out <- .makeGRaster(gns, names(x))
-	print("RESOLUTION MAY BE CHANGED!!!")
 
 	out
 
@@ -56,29 +111,51 @@ methods::setMethod(
 methods::setMethod(
 	f = "crop",
 	signature = c(x = "GVector"),
-	definition = function(x, y) {
-	
+	definition = function(x, y, ext = FALSE) {
+
 	compareGeom(x, y)
 	.restore(x)
 
-	## reshaping region to y
-	args <- list(
-		cmd = "g.region",
-		flags = c("quiet", "overwrite", "o"),
-		intern = TRUE
-	)
-
-	if (inherits(y, "GRaster")) {
-		args <- c(args, list(raster = .gnames(y)))
-	} else if (inherits(y, "GVector")) {
-		args <- c(args, list(vector = .gnames(y)))
-	}
-	
-	do.call(rgrass::execGRASS, args=args)
-
 	### crop
 	gn <- .makeGName("crop", "vector")
-	rgrass::execGRASS("v.clip", input=.gnames(x), clip=.gnames(y), output=gn, flags=c("quiet", "overwrite", "r"), intern=TRUE)
+	if (inherits(y, "GVector")) {
+
+		xgeom <- geomtype(x, grass = TRUE)
+		ygeom <- geomtype(y, grass = TRUE)
+
+		args <- list(
+			cmd = "v.clip",
+			input = .gnames(x),
+			output = gn,
+			flags = c("quiet", "overwrite"),
+			intern = TRUE
+		)
+		
+		if (ext) {
+			regionExt(y, respect = "dim")
+			args$flags <- c(args$flags, "r")
+			args$clip <- .gnames(y)
+		} else if (ygeom %in% c("point", "line")) {
+			ch <- convHull(y)
+			args$clip <- .gnames(ch)
+		} else if (ygeom == "area") {
+			args$clip <- .gnames(y)
+		}
+
+	} else if (inherits(y, "GRaster")) {
+
+		args <- list(
+			cmd = "v.clip",
+			input = .gnames(x),
+			clip = .gnames(y),
+			output = gn,
+			flags = c("r", "quiet", "overwrite"),
+			intern = TRUE
+		)
+
+	}
+
+	do.call(rgrass::execGRASS, args = args)
 
 	.makeGVector(gn)
 
