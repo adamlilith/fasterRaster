7235df635720b03248ade130197acb70483393cd
Adam B. Smith <adam@earthskysea.org>
2023-07-28 22:02:33 -0500

    Update tutorial_undocumented_functions.r

--- R/tutorial_undocumented_functions.r
+++ R/tutorial_undocumented_functions.r
@@ -2,39 +2,37 @@
 #'
 #' @title Undocumented functions
 #'
-#' @description **fasterRaster** interfaces with **GRASS GIS** to process rasters and spatial vector data. It is intended as an add-on to the **terra** and **sf** packages, and relies heavily on them. For most rasters and vectors that are small or medium-sized in memory/disk, those packages will almost always be faster. They may also be faster for very large objects.  But when they aren't, **fasterRaster** can step in.
-#'
-#' ## Installing **fasterRaster** has a number of undocumented functions. These are used internally, but may be of utility to power-users, so are documented here.
+#' @description **fasterRaster** has a number of undocumented functions. These are used internally, but may be of utility to power-users, so are documented here.
 #' 
-#' ### `.copyGSpatial()`
-#' Creates in **GRASS** a copy of a `GSpatial` object (a `GRaster` or a `GVector`).
+#' ## `.copyGSpatial()`
+#' Creates in **GRASS** a copy of a `GSpatial` object (typically, a `GRaster` or a `GVector`).
 #' 
-#' ### `dbName()` and `dbLayer()`
+#' ## `.dbName()` and `.dbLayer()`
 #' **GRASS** names of the data table and data layer associated with a vector.
 #'
-#' ### `.gnames()`
+#' ## `.gnames()`
 #' Gets the name(s) of the file(s) in **GRASS** that represent(s) the object(s).
 #'
-#' ### `.ls()`
+#' ## `.ls()`
 #' Lists the `gnames` of all objects in the current **GRASS** session.
 #'
-#' ### `.makeGNames()`
+#' ## `.makeGNames()`
 #' Makes one or more statistically unique strings that can be used as file names to represent rasters or vectors in **GRASS**.
 #' 
-#' ### `.refresh()`
+#' ## `.makeGRaster()` and `makeGVector()`
+#' Make `GRaster`s or `GVector`s from a vector of `gname`(s), which point to files in **GRASS**.
+#' 
+#' ## `.refresh()`
 #' Refresh metadata held within a `GRaster` or `GVector`` by querying the object in **GRASS**.
 #' 
-#' ### `.rename()`
+#' ## `.rename()`
 #' Rename a **GRASS** raster or vector.
 #' 
-#' ### `.rm()`
+#' ## `.rm()`
 #' Delete all **GRASS** files representing rasters or vectors.
 #' 
-#' ### `.rastInfo()`
-#' Metadata for a **GRASS** raster.
-#'
-#' ### `.vectInfo()`
-#' Metadata for a **GRASS** vector.
+#' ## `.rastInfo()` and `.vectInfo()`
+#' Metadata for a **GRASS** raster or vector.
 #'
 #' @keywords tutorial
 NULL
