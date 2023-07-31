599cc06952601c1883f64bd048b18d96d0105bcc
Adam B. Smith <adam@earthskysea.org>
2023-07-30 20:28:40 -0500

    Update aaa.r

--- R/aaa.r
+++ R/aaa.r
@@ -18,7 +18,8 @@
 	# packageStartupMessage("fasterRaster using, for example, "library(terra)".")
 	packageStartupMessage("To use fasterRaster functions, please connect to GRASS using faster().")
 	packageStartupMessage("Functions will be executed assuming you are using GRASS ", .grassVerDefault(), ".")
-	packageStartupMessage("Use setFastOptions() to set the GRASS version.")
+	packageStartupMessage("If you are using a more recent version, some functions will work faster if you")
+	packageStartupMessage("use setFastOptions() to set the version to match your GRASS installation.")
 
 	
 }
