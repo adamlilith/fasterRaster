994d81a8c378a0e8e2972a2a04ed8e34cd9f25ea
Adam B. Smith <adam@earthskysea.org>
2023-08-20 23:44:13 -0500

    Create madCover.r

---
+++ R/madCover.r
@@ -0,0 +1,21 @@
+#' @name madCover
+#'
+#' @title Raster of land cover for an eastern portion of Madagascar
+#'
+#' @description Raster of land cover for an eastern portion of Madagascar. Note that the land cover classes have been simplified, so this raster should *not* be used for "real" analyses.
+#'
+#' @docType data
+#'
+#' @format An object of class `SpatRaster` in unprojected (WGS84) coordinates.
+#'
+#' @keywords Madagascar
+#'
+#' @seealso [madCoverCats()], [categorical rasters][categorical_rasters]
+#'
+#' @references  Arino O., P. Bicheron, F. Achard, J. Latham, R. Witt and J.-L. Weber. 2008. GlobCover: The most detailed portrait of Earth. European Space Agency Bulletin 136:25-31. \url{http://due.esrin.esa.int}{http://due.esrin.esa.int}.
+#'
+#' @source \url{http://due.esrin.esa.int}{http://due.esrin.esa.int}
+#'
+#' @example man/examples/ex_fastData.r
+#'
+NULL
