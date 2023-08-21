d19eb510ebe64f1da84d1350d90aa6158bbf65e3
Adam B. Smith <adam@earthskysea.org>
2023-08-20 23:44:28 -0500

    Undo delete ncat()

--- R/ncat.r
+++ R/ncat.r
@@ -1,20 +1,48 @@
-#' Number of categories in a GRaster
+#' Number of levels in a categorical raster
 #'
-#' @description Reports number of categories in a categorical `GRaster`.
+#' @description This function reports the number of categories (levels) in a [categorical raster][tutorial_categorical_rasters].
 #'
-#' @param x A `GRaster`.
+#' @param x A `GRaster` or `SpatRaster`.
 #'
-#' @return A numeric vector with one value per layer in the `GRaster`.
-#' 
-#' @seealso [terra::levels()], [terra::cats()]
-#' 
-#' @example man/examples/ex_GRaster.r
+#' @param droplevels Logical: If `TRUE` (default), drop levels that do not appear in the raster.
+#'
+#' @return A numeric vector of integers. These represent the number of levels that appear in the raster (always for `GRaster`s, and for `SpatRaster`s when `dropLevels = TRUE`), or the number of levels that appear or could appear in the raster (for `SpatRaster`s only, when `dropLevels = FALSE`).
+#'
+#' @seealso [levels()], [terra::cats()], [terra::levels()], [terra::addCats()], [terra::droplevels()], [categorical rasters][tutorial_categorical_rasters]
+#'
+#' @example man/examples/ex_categorical_rasters.r
 #'
 #' @aliases ncat
 #' @rdname ncat
 #' @exportMethod ncat
 setMethod(
-	f = "ncat",
-	signature = "GRaster",
-	definition = function(x) x@nCats
+    f = "ncat",
+    signature = "GRaster",
+    definition = function(x) {
+        if (x@nCats == 0L) {
+            out <- 0L
+        } else {
+            levs <- levels(x)
+            isChar <- sapply(levs, inherits, "character")
+            out <- rep(NA_integer_, nlyr(x))
+            if (any(isChar)) out[isChar] <- 0L
+            if (any(!isChar)) out[!isChar] <- sapply(levs[!isChar], nrow)
+        }
+		names(out) <- names(x)
+        out
+    } # EOF
+)
+
+#' @aliases ncat
+#' @rdname ncat
+#' @exportMethod ncat
+setMethod(
+    f = "ncat",
+    signature = "SpatRaster",
+    definition = function(x, dropLevels = TRUE) {
+        if (dropLevels) x <- terra::droplevels(x)
+        out <- sapply(terra::levels(x), nrow)
+        names(out) <- names(x)
+        out
+    } # EOF
 )
