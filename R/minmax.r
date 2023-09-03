b2cb23cb2d5427880b8edebaf62a190e5c2c2ed4
Adam B. Smith <adam@earthskysea.org>
2023-09-02 23:24:02 -0500

    minmax() handles categorical rasters now

--- R/minmax.r
+++ R/minmax.r
@@ -1,10 +1,12 @@
 #' Minimum and maximum values or categories of a GRaster
 #'
-#' @description `minmax()` reports the minimum and maximum values across all non-NA cells of a `GRaster`. When the `cats` argument is `TRUE` and the raster is categorical, the function` reports the "lowest" and "highest" category values in a [categorical raster][tutorial_raster_data_types].
+#' @description `minmax()` reports the minimum and maximum values across all non-NA cells of a `GRaster`. When the `levels` argument is `TRUE` and the raster is categorical, the function` reports the "lowest" and "highest" category values in a [categorical raster][tutorial_raster_data_types].
 #'
 #' @param x A `GRaster`.
 #'
-#' @return `minmax()` returns a numeric matrix, and `minmax(..., cats = TRUE)` returns a `data.frame` with category names. In the latter case, non-categorical rasters will have `NA` values.
+#' @param levels Logical: If `TRUE` and the raster is a categorical raster, return the "lowest" and "highest" categories. The default is `FALSE`.
+#'
+#' @return `minmax()` returns a numeric matrix, and `minmax(..., levels = TRUE)` returns a `data.frame` with category names. In the latter case, non-categorical rasters will have `NA` values.
 #' 
 #' @seealso [terra::minmax()]
 #' 
@@ -16,7 +18,49 @@
 setMethod(
 	f = "minmax",
 	signature = "GRaster",
-	definition = function(x) matrix(c(x@minVal, x@maxVal), nrow=2, byrow=TRUE, dimnames=list(c("min", "max"), names(x)))
+	definition = function(x, levels = FALSE) {
+	
+	if (!levels) {
+	
+		out <- matrix(c(x@minVal, x@maxVal), nrow = 2, byrow = TRUE, dimnames = list(c("min", "max"), names(x)))
+
+	} else {
+
+		numLevels <- nlevels(x)
+		for (i in seq_len(nlyr(x))) {
+
+			if (numLevels[i] > 0L) {
+				
+				ac <- x@activeCat[[i]]
+    			cats <- x@levels[[i]][ , ..ac]
+				cats <- cats[[1L]]
+				thisMin <- cats[1L]
+				thisMax <- tail(cats, 1L)
+
+				this <- data.frame(TEMPTEMP_ = c(thisMin, thisMax), row.names = c("min", "max"))
+				
+				names(this) <- names(x)[i]
+
+			} else {
+
+				this <- data.frame(TEMPTEMP_ = c(NA_character_, NA_character_), row.names = c("min", "max"))
+				
+				names(this) <- names(x)[i]
+
+			}
+
+			if (i == 1L) {
+				out <- this
+			} else {
+				out <- cbind(out, this)
+			}
+
+		} # next raster
+
+	}
+	out
+
+	} # EOF
 )
 
 #' @noRd
@@ -33,42 +77,3 @@ methods::setMethod(
 	function(x) x@maxVal
 )
 
-#' @aliases minmaxCat
-#' @rdname minmax
-#' @exportMethod minmaxCat
-setMethod(
-    f = "minmaxCat",
-    signature = "GRaster",
-    definition = function(x) {
-	
-	numLevels <- nlevels(x)
-	for (i in seq_len(nlyr(x))) {
-
-		if (numLevels[i] > 0L) {
-   			
-			thisMin <- x@levels[[i]][[x@activeCat[i]]][1L]
-        	thisMax <- x@levels[[i]][[x@activeCat[i]]][numLevels[i]]
-
-			this <- data.frame(TEMPTEMP_ = c(thisMin, thisMax), row.names = c("min", "max"))
-		   	
-			names(this) <- names(x)[i]
-
-		} else {
-
-			this <- data.frame(TEMPTEMP_ = c(NA_character_, NA_character_), row.names = c("min", "max"))
-			
-			names(this) <- names(x)[i]
-
-		}
-
-		if (i == 1L) {
-			out <- this
-		} else {
-			out <- cbind(out, this)
-		}
-
-	} # next raster
-	out
-
-	} # EOF
-)
