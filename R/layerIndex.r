3457bc481c1a063e48337f6da31b6b4e1a723cfd
Adam B. Smith <adam@earthskysea.org>
2023-09-05 23:16:41 -0500

    Create layerIndex.r

---
+++ R/layerIndex.r
@@ -0,0 +1,24 @@
+#' Get index of raster layers
+#'
+#' @param layer Integer, numeric, logical, or character: Refers to one or more layers.
+#'
+#' @param x A `GRaster`.
+#'
+#' @param recycle Logical: If `TRUE` (default), and `layer` is logical and smaller in number than the number of layers, then recycle the vector of `layer`.
+#'
+#' @returns An integer vector.
+#'
+#' @noRd
+.layerIndex <- function(layer, x, recycle = TRUE) {
+
+	if (length(layer) > nlyr(x)) stop("Raster only contains ", nlyr(x), " layer(s).")
+
+	if (is.character(layer)) layer <- match(layer, names(x))
+	if (is.logical(layer)) {
+		if (recycle && length(layer) < nlyr(x)) layer <- rep(layer, length.out = nlyr(x))
+		layer <- which(layer)
+	}
+	if (any(!(layer %in% seq_along(nlyr(x))))) stop("Raster only contains ", nlyr(x), " layer(s).")
+	layer
+
+}
