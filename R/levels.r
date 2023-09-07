9bc153ebae968bf54bb653f4d560572f91f84863
Adam B. Smith <adam@earthskysea.org>
2023-09-06 23:07:48 -0500

    Put help in tutorial

--- R/levels.r
+++ R/levels.r
@@ -2,34 +2,19 @@
 #'
 #' @description `GRaster`s can represent [categorical data][tutorial_raster_data_types]. Cell values are actually integers, each corresponding to a category, such as "desert" or "wetland." A categorical raster is associated with a table that matches each value to a category name. The table must be `NULL` (i.e., no categories--so not a categorical raster), or have at least two columns. The first column must have integers and represent raster values. One or more subsequent columns must have category labels. The column with these labels is the "active category".
 #'
-#' Several functions help manage categorical rasters. **The functions described here return information about the "levels" table(s) of a categorical raster.**
-#'
-#' * `levels()`: Reports category values and their labels. Only the values and active column is reported.
-#' * `cats()`: Reports category values and their labels. The entire table is returned.
-#' * [levels<-]: Assign category values and their labels to all layers.
-#' * [categories()]: Assign category values and their labels to specific layers.
-#' * [addCats<-]: Add values and categories (rows) to the levels table of a categorical raster.
-#' * [addCats()]: Add new columns to a levels table using [data.table::merge()] or [cbind()]. This does not add new rows to the table.
-#' * `catNames()`: Column names of each levels table.
-#' * `missingCats()`: Values in the raster that have no assigned category.
-#' * [nlevels()]: Number of levels in each raster.
-#' * [activeCat()]: Column index or name of the category labels.
-#' * [activeCat<-]: Set the column of the category labels.
-#' * [droplevels()]: Removes levels that are not represented in the raster.
-#' * [freq()]: Frequency of each category across cells of a raster\cr
-#'
 #' @param x A `GRaster`.
 #'
 #' @param value A `data.frame`, `data.table`, a list of `data.frames` or `data.tables` with one per raster layer, or a categorical `SpatRaster`. The table's first column is the "value" column and must contain numeric values (of class `numeric` or `character`). If a `SpatRaster` is supplied, then its categories will be transferred to the `GRaster`.
 #'
 #' @param layer Numeric integers, logical vector, or character: Layer(s) for which to obtain levels.
 #'
-#' @returns Values returned are:
-#' * `levels()`: A list of `data.frame`s or `data.table`s, one per raster.
-#' * `cats()`: A list of `data.frame`s or `data.table`s, one per raster.
-#' * `activeCat()`: An integer vector or character vector.
+#' @param active An integer or a character: The index or column name of the column used for category labels (the "active column"). Following [terra::activeCat()], the first column of the "levels" table is ignored, so a value of 1 means to use the second column of the table for labels. A value of 2 means to use the third column, and so on.
 #'
-#' @seealso [levels<-], [catNames()], [missingCats()], [activeCat<-], [freq()], [nlevels()], [terra::cats()], [terra::levels()], [terra::addCats()], [terra::droplevels()], [categorical rasters][tutorial_raster_data_types] in **fasterRaster**
+#' @returns Values returned are:
+#' * `levels()` and `cats()`: A list of `data.frame`s or `data.table`s, one per raster.
+#' * `levels()<-` and `categories()`: A `GRaster`.
+#'
+#' @seealso [terra::levels()], [terra::levels<-], [terra::cats()], [terra::categories()], [categorical rasters][tutorial_raster_data_types] in **fasterRaster**
 #'
 #' @example man/examples/ex_GRaster_categorical.r
 #'
@@ -48,7 +33,7 @@ methods::setMethod(
 
 			if (numLevels[i] > 0L) {
 			
-				names <- catNames(x, i)
+				names <- catNames(x, i)[[1L]]
 				active <- activeCat(x, name = TRUE)[i]
 				value <- names[1L]
 
@@ -66,7 +51,7 @@ methods::setMethod(
 )
 
 #' @aliases cats
-#' @rdname levels
+#' @rdname categories
 #' @exportMethod cats
 methods::setMethod(
     f = "cats",
@@ -92,37 +77,151 @@ methods::setMethod(
     } # EOF
 )
 
-#' @aliases missingCats
+#' @aliases categories
 #' @rdname levels
-#' @exportMethod missingCats
+#' @exportMethod categories
 methods::setMethod(
-    f = "missingCats",
-    signature = c(x = "GRaster"),
-    function(x, layer = 1:nlyr(x)) {
+	f = "categories",
+	signature = c(x = "GRaster"),
+	function(x, layer = 1, value, active = 1) {
 
 	layer <- .layerIndex(layer, x, recycle = TRUE)
 
-	levs <- levels(x)
-	isFact <- is.factor(x)
+	if (!inherits(value, "list")) value <- list(value)
 
-	out <- list()
+	if (length(list) != length(layer)) stop("The number of level tables is not the same as the number of raster layers.")
+
 	for (i in layer) {
-	
-		if (!isFact[i]) {
-			out[[i]] <- numeric()
-		} else {
+
+		if (is.character(value[[i]]) && value[[i]] == "") {
+			value[[i]] <- data.table::data.table(NULL)
+		} else if (!inherits(value[[i]], "data.table")) {
+   			value[[i]] <- data.table::as.data.table(value[[i]])
+		}
+
+		# set value to integer and sort by value
+		if (!is.character(value[[i]])) {
+
+			names <- names(value[[i]])
+			valueCol <- names[1L]
+
+			# convert first column to integer
+			value[[i]][ , (valueCol) := lapply(.SD, as.integer), .SDcols = valueCol]
+
+			# detect non-unique values
+			unis <- unique(value[[i]][, 1L])
+			numUnis <- nrow(unis)
+			if (numUnis < nrow(value[[i]])) stop("The value column (the first column) must have unique values.")
+
+			# sort by first column
+			data.table::setorderv(value[[i]], col = valueCol)
 		
-			freqs <- freq(x[[i]])
+		}
 
-			ac <- activeCat(x, names = TRUE)[i]
-			val <- names(freqs)[1L]
+	}
 
-			out <- freqs[(is.na(get(ac))), get(val)]
+	for (i in layer) {
+		x@levels[[i]] <- value[[i]]
+		x@activeCat[i] <- as.integer(active)
+	}
+	methods::validObject(x)
+	x
 
-		} # if this layer has levels
-	} # next raster
-	names(out) <- names(x)[layer]
-	out
+	} # EOF
+)
 
-    } # EOF
+#' @aliases levels<-
+#' @rdname levels
+#' @exportMethod levels<-
+methods::setMethod(
+	f = "levels<-",
+	signature = c(x = "GRaster", value = "data.frame"),
+	function(x, value) {
+	
+	value <- list(value)
+	levels(x) <- value
+	
+	} # EOF
+)
+
+#' @aliases levels<-
+#' @rdname levels
+#' @exportMethod levels<-
+methods::setMethod(
+	f = "levels<-",
+	signature = c(x = "GRaster", value = "data.table"),
+	function(x, value) {
+	
+	value <- list(value)
+	levels(x) <- value
+	
+	} # EOF
+)
+
+#' @aliases levels<-
+#' @rdname levels
+#' @exportMethod levels<-
+methods::setMethod(
+	f = "levels<-",
+	signature = c(x = "GRaster", value = "SpatRaster"),
+	function(x, value) {
+
+	value <- levels(value)
+	levels(x) <- value
+	x
+
+	} # EOF
+)
+
+#' @aliases levels<-
+#' @rdname levels
+#' @exportMethod levels<-
+methods::setMethod(
+	f = "levels<-",
+	signature = c(x = "GRaster", value = "list"),
+	function(x, value) {
+
+	if (length(value) != nlyr(x)) stop("The number of level tables is not the same as the number of raster layers. \n  If you want to change the level table of select layers, use categories().")
+
+	for (i in seq_along(value)) {
+
+		# convert empty strings to NULL data.tables
+		if (is.character(value[[i]]) && value[[i]] == "") {
+
+			value[[i]] <- data.table::data.table(NULL)
+
+		# convert to data.table
+		} else if (!inherits(value[[i]], "data.table")) {
+			
+			value[[i]] <- data.table::as.data.table(value[[i]])
+
+		}
+
+		# set value to integer and sort by value
+		if (!is.character(value[[i]])) {
+			
+			names <- names(value[[i]])
+			valueCol <- names[1L]
+
+			# convert first column to integer
+			value[[i]][, (valueCol) := lapply(.SD, as.integer), .SDcols = valueCol]
+
+			# detect non-unique values
+			unis <- unique(value[[i]][ , 1L])
+			numUnis <- nrow(unis)
+			if (numUnis < nrow(value[[i]])) stop("The value column (the first column) must have unique values.")
+
+			# sort by first column
+   			data.table::setorderv(value[[i]], col = valueCol)
+
+		}
+
+	}
+
+	x@activeCat <- rep(2L, nlyr(x))
+	x@levels <- value
+ 	methods::validObject(x)
+	x
+
+	} # EOF
 )
