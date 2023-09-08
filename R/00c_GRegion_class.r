d3b6f2784f49adacf96c113e04d6d64aefae67dd
Adam B. Smith <adam@earthskysea.org>
2023-09-07 23:23:26 -0500

    Fix value order in @zextent

--- R/00c_GRegion_class.r
+++ R/00c_GRegion_class.r
@@ -19,11 +19,11 @@ setValidity("GRegion",
 		if (any(object@dimensions[1L:2L] <= 0L)) {
 			"First two values in @dimensions must be positive integers."
 		} else if (!is.na(object@dimensions[3L]) && object@dimensions[3L] <= 0L) {
-			"Third value in @dimensions must be NA or a positive integer."
+			"Third value in @dimensions must be NA_integer_ or a positive integer."
 		} else if (any(object@resolution[1L:2L] <= 0)) {
 			"First two values in @resolution must be positive real values."
 		} else if (!is.na(object@resolution[3L]) && object@resolution[3L] <= 0) {
-			"Third value in @resolution must be NA or a positive real value."
+			"Third value in @resolution must be NA_real_ or a positive real value."
 		} else {
 			TRUE
 		}
