6019c5031c137baf8dfc9017d28bc1ae9dd2a384
Adam B. Smith <adam@earthskysea.org>
2023-09-07 23:29:25 -0500

    Update tutorial_getting_started.r

--- R/tutorial_getting_started.r
+++ R/tutorial_getting_started.r
@@ -41,7 +41,7 @@
 #' library(sf)
 #' library(terra)
 #' ```
-#' Now, we need to start a **fasterRaster** "session", which makes a connection to **GRASS**. You generally only need to do this one time each time you start **R** and wish to use **fasterRaster**. We do this with the [faster()] function, which needs the `grassDir` argument and a `crs` argument. This latter sets the coordinate reference system (CRS) for the session--all rasters and vectors used in the session must have the same CRS, or be projected so they have this CRS.  You can start other sessions using `fasteStart()` with different CRSs, and switch between them using [fastRestore()].
+#' Now, we need to start a **fasterRaster** "session", which makes a connection to **GRASS**. You generally only need to do this one time each time you start **R** and wish to use **fasterRaster**. We do this with the [faster()] function, which needs the `grassDir` argument and a `crs` argument. This latter sets the coordinate reference system (CRS) for the session--all rasters and vectors used in the session must have the same CRS, or be projected so they have this CRS.  You can start other sessions using `fasteStart()` with different CRSs, and switch between them using [restoreSession()].
 #'
 #' The `crs` argument takes a coordinate reference string or an object from which such a string can be obtained. We will use a raster that comes with **fasterRaster** to set the `crs` argument.
 #' ```
