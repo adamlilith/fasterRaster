62818b232803b88796c7a3da5e46b7c052faf282
Adam B. Smith <adam@earthskysea.org>
2023-09-10 23:45:23 -0500

    Update as.contour.r

--- R/as.contour.r
+++ R/as.contour.r
@@ -4,10 +4,12 @@
 #' Create a `GVector` of contour lines from a `GRaster`.
 #'
 #' @param x A `GRaster`.
+#'
 #' @param nlevels Numeric: A positive integer or missing (default). Number of levels at which to calculate contours. Levels will be calculated in equal-sized steps from the smallest to the largest value of `x`. Either `nlevels` or `levels` must be specified.
+#'
 #' @param levels Numeric vector: A numeric vector of values at which to calculate contour lines. Either `nlevels` or `levels` must be specified.
 #'
-#' @seealso [terra::as.contour()], **GRASS** module `r.contour`
+#' @seealso [terra::as.contour()], module `r.contour` in **GRASS**
 #'
 #' @example man/examples/ex_asContour.r
 #'
@@ -19,14 +21,8 @@ setMethod(
 	signature(x = "GRaster"),
 	function(x, nlevels, levels) {
 	
-	### commons
-	###########
-	
 	.restore(x)
 	region(x)
-
-	### end commons
-	###############
 
 	if (!missing(nlevels) & !missing(levels)) stop("Please specify either ", sQuote("nlevels"), " or ", sQuote("levels"), ", but not both.")
 	if (missing(nlevels) & missing(levels)) stop("Please specify either ", sQuote("nlevels"), " or ", sQuote("levels"), ".")
@@ -54,4 +50,3 @@ setMethod(
 	
 	} # EOF
 )
-
