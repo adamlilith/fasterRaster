20e3e76ab4cb96e7ea03d0cc8d38294fe8029c8d
Adam B. Smith <adam@earthskysea.org>
2023-09-14 23:24:33 -0500

    Create dbCats.r

---
+++ R/dbCats.r
@@ -0,0 +1,21 @@
+#' Category column of a vector in GRASS
+#'
+#' @description Imports the `cat` column of a vector in **GRASS**. This is *not* the same as the `cat` index attached to a `GVector`, which is an **R** object that points to a vector in **GRASS**.
+#'
+#' @param x A `GVector`.
+#'
+#' @returns A `data.table` with a single column named `cat`.
+#'
+#' @aliases dbCats
+#' @aliases dbCats
+#' @noRd
+methods::setMethod(
+	f = "dbCats",
+	signature = c(x = "GVector"),
+	definition = function(x) {
+
+	out <- dbToDataTable(x)
+	out[ , "cat"]
+
+	} # EOF
+)
