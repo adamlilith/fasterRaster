aa9f4aef65f9adc5096e2793c4b3d97d0ed76449
Adam B. Smith <adam@earthskysea.org>
2023-09-14 23:25:28 -0500

    Added $, $<- for rast & vect; vector subset/assigns work with data.table

--- R/subset_assign.r
+++ R/subset_assign.r
@@ -1,14 +1,15 @@
 #' Sub-setting operator for GRasters and GVectors
 #'
-#' @description The `[` and `[[` operators do different things depending on whether they are applied to a `GRaster` or `GVector`:
+#' @description The `[` and `[[` operators do different things depending on whether they are applied to a `GRaster` or `GVector` and whether the index is positive or negative. Positive indices select the given geometry/row, column, or layer. Negative indices remove them. In general, indices can be numeric integers, integers, logical vectors, or character vectors.
 #' * `GVector`s:
-#'     * `[` operator: Returns a subset of geometries (i.e., points, lines, or polygons) of the `GVector`. For example, `vector[2:4]` will return the second through the fourth geometries.
-#'     * `[[` operator: Returns a vector with the selected columns in its data frame. For example, `vector[[2:4]]` returns the `GVector`, but with just columns 2 through 4 of the data frame.
+#'     * `[` operator: Returns a subset of geometries (i.e., points, lines, or polygons) of the `GVector`. For example, `vector[2:4]` will return the second through the fourth geometries. `vector[2:4, 1]` returns a vector with the second and third geometries and the first column of its attribute table.
+#'     * `$` and `[[` operator: Returns a vector with the selected columns in its data frame. For example, `vector[[2:4]]` returns the `GVector`, but with just columns 2 through 4 of its data frame. `vector$State` returns the values in the `State` column.
+#'     * `$<-` and `[[<-` operator: Replaces the values in the column(s) indicated by the index by the new value. For example, `vector$State <- "Kansas"` replaces all values in the `State` column with heaven.
 #' * `GRaster`s:
-#'     * `[` operator with the assignment operator, `<-`: Assigns the given value to all cells in the `GRaster`. For examples, `raster[] <- 7` replaces all cell values in `raster` with 7.
-#'     * `[[` operator: Returns `GRaster`s from a "[stack][c]" of `GRaster`s. For example, `raster[[2:3]]` returns the second and third rasters in a stack of `GRaster`s, and `raster[[c("bio1", "bio12")]]` returns the two rasters with [names()] "bio1" and "bio12".
-#'     * `[[` operator with the assignment operator, `<-`: Replaces existing layers or adds new layers to a `GRaster`. For example, `x[[2]] <- newRaster` makes the second layer of `x` `newRaster`. If `x` already has two layers, the second one is replaced. If it only had one layer, a second is added.
-#'     * `add<-`: "Stacks" two `GRaster`s. This is the same as stacking rasters with [c()]. For example, you `c(raster1, raster2)` and `add(raster1) <- raster2` are equivalent.
+#'     * `$` and `[[` operators: Returns `GRaster`s from a "[stack][c]" of `GRaster`s. For example, `raster[[2:3]]` returns the second and third rasters in a stack of `GRaster`s, `raster[[c("bio1", "bio12")]]` returns the two rasters with [names()] "bio1" and "bio12", and `raster$bio12` returns the `bio12` raster.
+#'     * `[<-` operator with the assignment operator, `<-`: Assigns the given value to all cells in the `GRaster`. For examples, `raster[] <- 7` replaces all cell values in `raster` with 7.
+#'     * `$<-` and [[<-` operators: Replaces existing layers or adds new layers to a `GRaster`. For example, `x[[2]] <- newRaster` makes the second layer of `x` `newRaster`. If `x` already has two layers, the second one is replaced. If it only had one layer, a second is added. `x$bio12 <- newRaster` replaces the layer named `bio12` with `newRaster`.
+#'     * `add<-`: "Stacks" two `GRaster`s. This is the same as stacking rasters with [c()]. For example, `c(raster1, raster2)` and `add(raster1) <- raster2` are equivalent.
 #'
 #' @param x A `GVector`, or a `GRaster` with one or more layers.
 #'
@@ -64,7 +65,39 @@ methods::setMethod(
 	} # EOF
 )
 
-#' @title Sub-setting assignment operator for GRasters and GVectors
+#' @name subset_assign
+#' @aliases subset_assign
+#' @rdname subset_assign
+#' @exportMethod $
+methods::setMethod(
+    "$",
+    signature = c(x = "GRaster"),
+    function(x, name) x[[name]]
+)
+
+#' @name subset_assign
+#' @aliases subset_assign
+#' @rdname subset_assign
+#' @exportMethod $<-
+methods::setMethod(
+    "$<-",
+    signature = c(x = "GRaster"),
+    function(x, name, value) {
+
+		if (is.logical(name)) {
+			if (length(name) < nlyr(x)) name <- rep(name, length.out = nlyr(x))
+			i <- which(name)
+		} else if (is.character(name)) {
+			i <- match(name, names(x))
+		}
+
+		x[[name]] <- value
+		names(x)[i] <- names(value)
+		x
+
+	} # EOF
+)
+
 #' @aliases [[<-
 #' @rdname subset_assign
 #' @exportMethod [[<-
@@ -156,11 +189,11 @@ methods::setMethod(
 			maxVal <- .maxVal(out)
 			maxVal[insides] <- .maxVal(value)[valueInsides]
 
-			ac <- out@activeCat
+			ac <- activeCat(out, layer=seq_len(nlyr(x)))
 			ac[insides] <- activeCat(value)[valueInsides]
 
 			levs <- levels(out)
-			levs[insides] <- levels(value)[levs]
+			levs[insides] <- levels(value)[valueInsides]
 
 			out <- new(
 				"GRaster",
@@ -202,6 +235,16 @@ methods::setMethod(
 	f = "add<-",
 	signature = c(x = "GRaster", value = "GRaster"),
 	function(x, value) c(x, value)
+)
+
+#' @name subset_assign
+#' @aliases subset_assign
+#' @rdname subset_assign
+#' @exportMethod $<-
+methods::setMethod(
+    "$<-",
+    signature = c(x = "GRaster"),
+    function(x, name, value) x[[name]] <- value
 )
 
 #' @rdname subset_assign
@@ -251,36 +294,178 @@ methods::setMethod(
 	signature = c(x = "GVector"),
 	function(x, i) {
 
-	nr <- nrow(x)
-	if (is.logical(i)) i <- which(i)
-	if (any(i < 1L | i > nr)) stop("Index out of bounds.")
+	.restore(x)
 
-	# select rows to drop	
-	rows <- seq_len(nr)
-	drops <- rows[!(rows %in% i)]
-
-	if (length(i) >= nr) {
-		where <- paste0("cat IN (", paste0(drops, collapse = ","), ")")
-	} else {
-		where <- paste0("cat NOT IN (", paste0(i, collapse = ","), ")")
+	nr <- ngeom(x)
+	if (is.logical(i)) {
+		if (length(i) < nr) i <- rep(i, length.out = nr)
+		i <- which(i)
 	}
-	
-cat("Command line too long when selecting > ~1500 records.")
-utils::flush.console()
 
-	src <- .makeSourceName(NULL, rastOrVect = "vector")
-	args <- list(
-		cmd = "v.db.droprow",
-		input = sources(x),
-		layer = .dbLayer(x),
-		output = src,
-		where = where,
-		flags = c("quiet", "overwrite"),
-		intern = FALSE
-	)
+	if (any(i > 0L) & any(i < 0L)) stop("Cannot mix positive and negative indices.")
 
-	do.call(rgrass::execGRASS, args=args)
-	.makeGVector(src)
+	if (all(i < 0L)) {
+		reverseSelect <- TRUE
+		iRev <- i
+		i <- -1L * i
+		removeAll <- length(i) == nrow(x) && all(sort(i) == seq_len(nrow(x)))
+	} else {
+		reverseSelect <- removeAll <- FALSE
+	}
+	if (any(i > nr)) stop("Index out of bounds.")
+
+	if (removeAll) {
+		out <- NULL # removed all rows
+	} else {
+
+		# **keep** rows
+		iSeq <- seqToSQL(i)
+		src <- .makeSourceName("v_extract", "vector")
+
+		args <- list(
+			cmd = "v.extract",
+			input = sources(x),
+			cats = iSeq,
+			output = src,
+			new = 1,
+			flags = c("quiet", "overwrite", "t"),
+			intern = FALSE
+		)
+
+		if (reverseSelect) args$flags <- c(args$flags, "r")
+		do.call(rgrass::execGRASS, args = args)
+		
+		if (nrow(x) == 0L) {
+			table <- NULL
+		} else if (reverseSelect) {
+			table <- x@table[iRev]
+		} else {
+			table <- x@table[i]
+		}
+
+		out <- .makeGVector(src, table = table)
+
+	} # not removing all rows
+	out
+
+	} # EOF
+)
+
+#' @aliases subset_assign
+#' @rdname subset_assign
+#' @exportMethod [
+methods::setMethod(
+	"[",
+	signature = c(x = "GVector"),
+	function(x, i, j) {
+
+	.restore(x)
+
+	nr <- ngeom(x)
+
+	if (missing(i)) {
+		out <- x[[j]]
+	} else {
+
+		if (is.logical(i)) {
+			if (length(i) < nr) i <- rep(i, length.out = nr)
+			i <- which(i)
+		}
+
+		if (any(i > 0L) & any(i < 0L)) stop("Cannot mix positive and negative indices.")
+
+		if (all(i < 0L)) {
+			reverseRowSelect <- TRUE
+			iRev <- i
+			i <- -1L * i
+			removeAll <- length(i) == nrow(x) && all(sort(i) == seq_len(nrow(x)))
+
+		} else {
+			reverseRowSelect <- removeAll <- FALSE
+		}
+		if (any(i > nr)) stop("Index out of bounds.")
+
+		if (removeAll) {
+			out <- NULL # removed all rows
+		} else {
+
+			# **keep** rows
+			iSeq <- seqToSQL(i)
+			src <- .makeSourceName("v_extract", "vector")
+
+			args <- list(
+				cmd = "v.extract",
+				input = sources(x),
+				cats = iSeq,
+				output = src,
+				new = 1,
+				flags = c("quiet", "overwrite", "t"),
+				intern = FALSE
+			)
+
+			if (reverseRowSelect) args$flags <- c(args$flags, "r")
+			do.call(rgrass::execGRASS, args = args)
+			
+			if (nrow(x) == 0L) {
+				table <- NULL
+			} else {
+
+				table <- x@table
+
+				# select columns
+				if (missing(j)) {
+					removeAllCols <- FALSE
+				} else {
+
+					nc <- ncol(table)
+					if (is.character(j)) {
+						j <- match(j, names(table))
+					} else if (is.logical(j)) {
+						if (length(j) < nc) j <- rep(j, length.out = nc)
+						j <- which(j)
+					}
+					
+					if (all(j < 0L)) {
+						reverseColSelect <- TRUE
+						j <- j * -1L
+						removeAllCols <-  (all(sort(j) == seq_len(ncol(x))))
+					} else {
+						reverseColSelect <- removeAllCols <- FALSE
+					}
+
+					if (any(j > nc)) stop("Index out of bounds.")
+
+					if (removeAllCols) {
+						table <- data.table::data.table(NULL)
+					} else {
+					
+						if (reverseColSelect) {
+							j <- setdiff(names(x), names(x)[j])
+						} else {
+							j <- names(x)[j]
+						}
+						table <- table[ , ..j]
+
+					}
+				
+				}
+
+				# select rows
+				if (!removeAllCols) {
+					if (reverseRowSelect) {
+						table <- table[iRev]
+					} else {
+						table <- table[i]
+					}
+				}
+			
+			} # vector has table
+			out <- .makeGVector(src, table = table)
+
+		} # keep some rows (vs discarding all)
+
+	} # if selecting some rows
+	out
 
 	} # EOF
 )
@@ -294,32 +479,66 @@ methods::setMethod(
 	function(x, i) {
 
 	nc <- ncol(x)
+	if (nc == 0L) stop("Vector has no data table associated with it.")
+
+	# turn i into integer
 	if (is.logical(i)) {
+		if (length(i) <- nc) i <- rep(i, length.out = nc)
 		i <- which(i)
 	} else if (is.character(i)) {
-		misses <- !(i %in% names(x))
-		if (any(misses)) stop("At least one named column does not exist in this vector\"s data table.")
-		i <- which(names(x) %in% i)
+		i <- match(i, names(x))
+  		if (anyNA(i)) stop("At least one named column does not exist in this vector\'s data table.")
 	}
 
-	if (any(i < 1L | i > nc)) stop("Index out of bounds.")
+	if (any(i < 0L) & any(i > 0L)) stop("Cannot have both positive and negative indices.")
+
+	if (all(i < 0L)) {
+		reverseSelect <- TRUE
+		i <- i * -1L
+		removeAll <- length(i) == ncol(x) && all(sort(i) == seq_len(ncol(x)))
+	} else {
+		reverseSelect <- removeAll <- FALSE
+	}
+
+	if (any(i > nc)) stop("Index out of bounds.")
 	
-	cols <- seq_len(nc)
-	drops <- cols[!(cols %in% i)]
-	drops <- names(x)[drops]
+	# removed all columns
+	if (removeAll) {
+		x@table <- data.table::data.table(NULL)
+	# keep some columns
+	} else {
+		if (reverseSelect) {
+			i <- setdiff(names(x), names(x)[i])
+		} else {
+			i <- names(x)[i]
+		}
 
-	src <- .copyGSpatial(x)
-	args <- list(
-		cmd = "v.db.dropcolumn",
-		map = src,
-		columns = drops,
-		flags = "quiet",
-		layer = "1",
-		intern = FALSE
-	)
+		x@table <- x@table[ , ..i]
+	}
+	x
+	
+	} # EOF
+)
 
-	do.call(rgrass::execGRASS, args=args)
-	.makeGVector(src)
+#' @aliases subset_assign
+#' @rdname subset_assign
+#' @exportMethod $
+methods::setMethod(
+	"$",
+	signature = c(x = "GVector"),
+	function(x, name) x@table[[name]]
+)
+
+#' @aliases subset_assign
+#' @rdname subset_assign
+#' @exportMethod $<-
+methods::setMethod(
+	"$<-",
+	signature = c(x = "GVector"),
+	function(x, name, value) {
+
+	x@table <- data.table::set(x@table, i = NULL, j = name, value = value)
+	x
 
 	} # EOF
 )
