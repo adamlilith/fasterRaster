e2201120f408134bcceb7bb137c104693ac10c13
Adam B. Smith <adam@earthskysea.org>
2023-09-18 22:55:32 -0500

    Create update.r

---
+++ R/update.r
@@ -0,0 +1,27 @@
+#' Refresh metadata in a GRaster or GVector
+#'
+#' `GRaster`s and `GVector`s are really pointers to objects in **GRASS**. The values displayed when you use `show()` or `print()` for a `GRaster` or `GVector` are stored in **R**. If, on the odd chance that you make a change to a `GRaster` or `GVector` (e.g., using commands in the **rgrass** package), the changes will not be automatically reflected in the `GRaster` or `GVector`. This function can be used to update the objects in **R** to reflect their proper values.
+#'
+#' @param object A `GRaster` or `GVector`.
+#'
+#' @returns A `GRaster` or `GVector`.
+#'
+#' @example man/examples/ex_GRaster.r 
+#'
+#' @aliases update
+#' @rdname update
+#' @exportMethod update
+methods::setMethod(
+    f = "update",
+    signature = c(object = "GRaster"),
+    definition = function(object) .makeGRaster(sources(object))
+)
+
+#' @aliases update
+#' @rdname update
+#' @exportMethod update
+methods::setMethod(
+    f = "update",
+    signature = c(object = "GVector"),
+    definition = function(object) .makeGVector(sources(object))
+)
