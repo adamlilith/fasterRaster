31e546d91b2b9c4a653acad246514eb42ae30925
Adam B. Smith <adam.smith@mobot.org>
2023-09-21 23:06:56 -0500

    Create denoise() noise()

--- R/denoise_noise.r
+++ R/denoise_noise.r
@@ -4,11 +4,11 @@
 #' 
 #' `noise()` does the opposite by first constructing the PCA, predicting values back to the raster, then subtracting these values from the original, removing coarse-scale trends and thereby leaving "noise".
 #' 
-#' Note that by adding each raster layer from the output of `denoise()` to each raster layer from `noise()`, you will get the original input rasters back. Thus, these two functions together simply decompose signals in the input between coarse-scale and fine-scale trends.
-#' 
 #' @param x A `GRaster` with two or more layers.
 #' 
 #' @param scale Logical: If `TRUE`, input layers will be rescaled by dividing each layer by its overall population standard deviation. Note that rasters will always be centered (have their mean subtracted from values). Centering and scaling is recommended when rasters values are in different units. The default is `FALSE` (do not scale).
+#' 
+#' @param percent Numeric integer or integer in the range 50 to 99 (default is 80): Minimum total variation explained in the retained PC axes. Higher values will cause `denoise()` to remove less noise, and `noise()` to return less noise. If this value to too low to retain even one axis, the function will fail with a message to that effect.
 #' 
 #' @returns A multi-layer `GRaster` with one layer per input.
 #' 
@@ -22,9 +22,16 @@
 methods::setMethod(
 	f = "denoise",
 	signature = c(x = "GRaster"),
-	function(x, scale = FALSE) {
+	function(x, scale = FALSE, percent = 80) {
 
 	if (nlyr(x) < 2L) stop("Input raster must have 2 or more layers.")
+
+	if (percent %% 1 != 0) {
+		warning("Argument ", sQuote("percent"), " must be an integer. Value will be rounded.")
+		percent <- round(percent)
+	}
+
+	if (percent < 50 | percent > 99) stop("Argument ", sQuote("percent"), " must be an integer in the range [50, 99].")
 
 	.restore(x)
 	region(x)
@@ -37,6 +44,7 @@ methods::setMethod(
 		input = input,
 		output = src,
 		rescale = c(0, 0),
+		percent = percent,
 		flags = c("quiet", "overwrite", "f"),
 		intern = TRUE
 	)
@@ -58,9 +66,9 @@ methods::setMethod(
 methods::setMethod(
 	f = "noise",
 	signature = c(x = "GRaster"),
-	function(x, scale = FALSE) {
+	function(x, scale = FALSE, percent = 80) {
 
-	denoised <- denoise(x, scale = scale)
+	denoised <- denoise(x, scale = scale, percent = 80)
 	out <- x - denoised
 	names(out) <- names(x)
 	out
