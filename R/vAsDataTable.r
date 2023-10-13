505ab073beb4ea4cbed00f087f05c2bc1ad0b07d
Adam B. Smith <adam.smith@mobot.org>
2023-10-12 23:16:24 -0500

    Test if db exists first

--- R/vAsDataTable.r
+++ R/vAsDataTable.r
@@ -1,6 +1,6 @@
 #' Convert the attribute table of a vector to a data.table
 #'
-#' @description All **GRASS** vectors have attribute tables with the first column named `cat` and holding integer values indexing each feature. This table is mostly independent from the **fasterRaster** attribute table, which is stored as a `data.table` in the `@table` slot of a `GRaster`. This function exports the **GRASS** attribute table to **R**.
+#' @description **GRASS** vectors typically have a database with the first column named `cat` and holding integer values indexing each feature. This table is mostly independent from the **fasterRaster** attribute table, which is stored as a `data.table` in the `@table` slot of a `GRaster`. This function exports the **GRASS** database to **R**.
 #'
 #' Values in the `cat` column are not necessarily unique--if a value appears more than once, the set of features they index are (in other software) called "multipart" features. The table can have more columns with metadata for each feature.
 #'
@@ -8,7 +8,7 @@
 #'
 #' @param x A `GVector` or the name of a vector in **GRASS**.
 #'
-#' @returns A `data.table`.
+#' @returns A `data.table` or `NULL` if the vector is not attached to a database.
 #'
 #' @aliases .vAsDataTable
 #' @rdname dbToDataTable
@@ -22,68 +22,24 @@
 		src <- x
 	}
 
-	# data <- rgrass::execGRASS("v.db.select", map = src, intern = TRUE, separator = "pipe", null_value = "NA")
-	tf <- tempfile(fileext = ".csv")
-	rgrass::execGRASS(
-		"v.db.select",
-		map = src,
-		intern = TRUE,
-		separator = "comma",
-		null_value = "NA",
-		file = tf,
-		flags = "overwrite"
-	)
-	out <- data.table::fread(tf)
+	hasTable <- .vHasTable(src)
+	if (!hasTable) {
+		out <- NULL
+	} else {
 
-	# ### "data" can be
-	# # * a single vector headed by "cat" and no pipe characters if only this column is available
-	# # * a vector with pipe characters if more than one column is available
-
-	# cols <- data[1L]
-	# data <- data[-1L]
+		tf <- tempfile(fileext = ".csv")
+		rgrass::execGRASS(
+			"v.db.select",
+			map = src,
+			intern = TRUE,
+			separator = "comma",
+			null_value = "NA",
+			file = tf,
+			flags = "overwrite"
+		)
+		out <- data.table::fread(tf)
 	
-	# ### one column
-	# if (!grepl(data[1L], pattern = "\\|")) {
-	
-		# if (any(grepl(data, pattern = "\\|"))) data <- gsub(data, pattern = "\\|", replacement = "")
-		# out <- data.table::data.table(cat = as.integer(data))
-	
-	# ### multiple columns
-	# } else {
-	
-		# cols <- strsplit(cols, "\\|")[[1L]]
+	}
 
-		# # data values
-		# data <- strsplit(data, "\\|")
-		# lengths <- sapply(data, length)
-		# shorts <- which(lengths < length(cols))
-		# for (short in shorts) data[[short]][length(data[[short + 1L]])] <- ""
-		
-		
-		# out <- do.call(rbind.data.frame, data)
-		
-		# # add NAs in missing columns
-		# diff <- length(cols) - ncol(out)
-		# if (diff > 0L) {
-			
-			# empty <- matrix(NA, ncol = diff, nrow = nrow(out))
-
-			# out <- cbind(out, empty)
-		
-		# }
-		
-		# out <- data.table::as.data.table(out)
-		# colnames(out) <- cols
-
-		# for (i in seq_len(ncol(out))) {
-
-			# this <- out[[i]]
-			# if (!all(is.na(this)) && all(this == "")) {
-				# out[ , (i)] <- NA
-			# }
-
-		# }
-
-	# }
 	out
 }
