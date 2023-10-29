9441eb92d36a7a12740fbbd5173f4349de1eb18d
Adam B. Smith <adam.smith@mobot.org>
2023-10-29 01:14:55 -0500

    Reports geometry cats by default (not table cats)

--- R/vCats.r
+++ R/vCats.r
@@ -4,14 +4,16 @@
 #'
 #' @param x A `GVector` or the name of a vector in **GRASS**.
 #'
-#' @param long Logical: If `TRUE`, return category numbers for all sub-geometries. If `FALSE` (default), return category numbers for all geometries.
+#' @param table Logical: If `TRUE`, return category numbers from the data table associated with the vector. If `FALSE` (default), return category numbers from the actual vector.
+#'
+#' @param integer Logical: If `TRUE` (default), return category values as integers. In some cases, a geometry can have multiple categories, in which case `NA` is returned. If `FALSE`, return category values as strings (and thus, if a geometry has more than one category, does not convert to `NA`).
 #'
 #' @returns An integer vector.
 #'
 #' @aliases .vCats
 #' @rdname vCats
 #' @noRd
-.vCats <- function(x, long = FALSE) {
+.vCats <- function(x, table = FALSE, integer = TRUE) {
 
 	if (inherits(x, "GVector")) {
 		.restore(x)
@@ -20,16 +22,12 @@
 		src <- x
 	}
 
-	if (long) {
-
-		out <- rgrass::execGRASS("v.category", input = src, option = "print", flags = "quiet", intern = TRUE)
-
-		as.integer(out)
-
+	if (table) {
+		out <- .vAsDataTable(src)$cat
 	} else {
-
-		as.integer(.vAsDataTable(src)$cat)
-	
+		out <- rgrass::execGRASS("v.category", input = src, option = "print", flags = "quiet", intern = TRUE)
 	}
+	if (integer) out <- as.integer(out)
+	out
 
 }
