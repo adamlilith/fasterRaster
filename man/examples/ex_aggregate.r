787030fdf8e104ee99c19d1e95a16bdb023d764b
Adam B. Smith <adam.smith@mobot.org>
2023-11-02 23:06:16 -0500

    Update ex_aggregate.r

--- man/examples/ex_aggregate.r
+++ man/examples/ex_aggregate.r
@@ -20,15 +20,19 @@ library(terra)
 
 # Example data
 madElev <- fastData("madElev")
+madCoast4 <- fastData("madCoast4")
 
 # Start GRASS session for examples only:
 faster(x = madElev, grassDir = grassDir,
 workDir = tempdir(), location = "examples") # line only needed for examples
 
-# Convert a SpatRaster to a GRaster:
+### aggregating a GRaster
+#########################
+
+# Convert:
 elev <- fast(madElev)
 
-### Aggregate by same factor in 2 dimensions
+### Aggregate GRaster by same factor in 2 dimensions
 # fasterRaster
 agg2 <- aggregate(elev, 2, "mean")
 agg2
@@ -37,12 +41,13 @@ agg2
 agg2terra <- aggregate(madElev, 2, "mean")
 agg2terra
 
-# Compare fasterRaster and terra... These should be the same.
+# Compare rasters aggregated by fasterRaster and terra.
+# These should be the same.
 agg2 <- rast(agg2)
 agg2 <- extend(agg2, agg2terra)
 agg2 - agg2terra
 
-### Aggregate by a non-integer factor in 2 dimensions
+### Aggregate GRaster by a non-integer factor in 2 dimensions
 # fasterRaster
 agg2.9 <- aggregate(elev, 2.9, "mean")
 agg2.9
@@ -51,14 +56,33 @@ agg2.9
 agg2.9terra <- aggregate(madElev, 2.9, "mean")
 agg2.9terra
 
-# Compare fasterRaster and terra... These should be different.
+# Compare rasters aggregated by fasterRaster and terra.
+# These should be different.
 res(agg2.9)
 res(agg2.9terra) # terra rounds aggregation factor down
 2 * res(madElev) # original resolution multipled by 2
 
-### Aggregate by different factor in 2 dimensions
+### Aggregate GRaster by different factor in 2 dimensions
 agg2x3 <- aggregate(elev, c(2, 3), "mean")
 agg2x3
+
+### aggregating a GVector
+#########################
+
+# Convert:
+coast <- fast(madCoast4)
+
+# Aggregate:
+aggCoast <- aggregate(coast)
+
+ngeom(coast)
+ngeom(aggCoast)
+
+# plot
+oldpar <- par(mfrow = c(1, 2))
+plot(coast, main = "Original")
+plot(aggCoast, main = "Aggregated")
+par(oldpar)
 
 # IMPORTANT #3: Revert back to original GRASS session if needed.
 restoreSession(opts.)
