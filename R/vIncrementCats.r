af99dc766f0920e31f5aec367a2c2d7db08a97ea
Adam B. Smith <adam@earthskysea.org>
2023-11-04 15:31:26 -0500

    Create vIncrementCats.r

---
+++ R/vIncrementCats.r
@@ -0,0 +1,41 @@
+#' Increment category values of a GVector
+#'
+#' @description Adds a constant to all category values of a `GVector`.
+#'
+#' @param x A `GVector` or the [sources()] name of one.
+#'
+#' @param add Integer: Value to add to each category value.
+#'
+#' @param return Character: What to return. Either `"GVector"` or "`sources`" ([sources()] name of the new vector).
+#'
+#' @returns The same `GVector` as in `x`, but with incremented category values.
+#'
+#' @aliases .vIncrementCats
+#' @noRd
+.vIncrementCats <- function(x, add, return = "GVector") {
+
+	if (inherits(x, "GVector")) {
+		.restore(x)
+		srcIn <- sources(x)
+	} else {
+		srcIn <- x
+	}
+
+	src <- .makeSourceName("v_category", "vector")
+	rgrass::execGRASS(
+		cmd = "v.category",
+		input = srcIn,
+		output = src,
+		option = "sum",
+		cat = add,
+		flags = c("quiet", "overwrite")
+	)
+
+	return <- pmatchSafe(return, c("GVector", "sources"))
+	if (return == "GVector") {
+		.makeGVector(src, table = as.data.table(x))
+	} else {
+		src
+	}
+
+}
