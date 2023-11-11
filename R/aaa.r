587603a0d7acc00a58296a96c215306830b396dc
Adam B. Smith <adam.smith@mobot.org>
2023-11-11 00:48:51 -0600

    Remove messages

--- R/aaa.r
+++ R/aaa.r
@@ -17,14 +17,10 @@
 	.fasterRaster$locations <- list()
 	setFastOptions(restore = TRUE)
 	
-	ver <- read.dcf(file=system.file("DESCRIPTION", package=pkg), fields="Version")
+	ver <- read.dcf(file=system.file("DESCRIPTION", package = pkg), fields = "Version")
 	packageStartupMessage(paste(pkg, ver))
 	# packageStartupMessage("It is recommended to attach the terra and sf packages before")
 	# packageStartupMessage("fasterRaster using, for example, "library(terra)".")
-	packageStartupMessage("To use fasterRaster functions, please connect to GRASS using faster().")
-	packageStartupMessage("Functions will be executed assuming you are using GRASS ", .grassVerDefault(), ".")
-	packageStartupMessage("If you are using a more recent version, some functions will work faster if you")
-	packageStartupMessage("use setFastOptions() to set the version to match your GRASS installation.")
-
+	# packageStartupMessage("To use fasterRaster functions, please connect to GRASS using faster().")
 	
 }
