77dd4d9f3734833d37d0c379fd23d47846d1b94c
Adam B. Smith <adam.smith@mobot.org>
2023-11-11 00:49:44 -0600

    Add extraction using lines/polygons

--- man/examples/ex_extract.r
+++ man/examples/ex_extract.r
@@ -19,34 +19,39 @@ library(sf)
 library(terra)
 
 # Example data: elevation raster and points vector
-madElev <- fastData("madElev")
-madCover <- fastData("madCover")
-madCoast4 <- fastData("madCoast4")
-madDypsis <- fastData("madDypsis")
+madElev <- fastData("madElev") # raster
+madCover <- fastData("madCover") # categorical raster
+madDypsis <- fastData("madDypsis") # points vector
+madRivers <- fastData("madRivers") # lines vector
+madCoast4 <- fastData("madCoast4") # polygons vector
 
 # start GRASS session for examples only
 faster(x = madElev, grassDir = grassDir,
 workDir = tempdir(), location = "examples") # line only needed for examples
 
 # Convert to fasterRaster formats:
-elev <- fast(madElev)
-cover <- fast(madCover)
-coast <- fast(madCoast4)
-dypsis <- fast(madDypsis)
+elev <- fast(madElev) # raster
+cover <- fast(madCover, method = "near", warn = FALSE) # categorical raster
+dypsis <- fast(madDypsis) # points vector
+rivers <- fast(madRivers) # lines vector
+coast <- fast(madCoast4) # polygons vector
 
 # Get values of elevation at points where Dypsis species are located:
 extract(elev, dypsis, xy = TRUE)
 
-# Extract from categorical raster:
-vals <- extract(cover, dypsis)
-vals
+# Extract from categorical raster at points:
+extract(cover, dypsis)
+extract(cover, dypsis, cats = TRUE)
 
-cats <- extract(cover, dypsis, cats = TRUE)
-cats
+# Extract and summarize values on a raster across polygons:
+extract(elev, coast, fun = c("sum", "mean", "countNonNA"), overlap = FALSE)
 
-# Extract from a vector:
+# Extract and summarize values on a raster across lines:
+extract(elev, rivers, fun = c("sum", "mean", "countNonNA"), overlap = FALSE)
+
+# Extract from a polygons vector at a points vector:
 table <- extract(coast, dypsis, xy = TRUE)
-head(table) # first 3 are outside vector, next 3 are inside
+head(table) # first 3 are outside polygons vector, next 3 are inside
 
 # IMPORTANT #3: Revert back to original GRASS session if needed.
 restoreSession(opts.)
