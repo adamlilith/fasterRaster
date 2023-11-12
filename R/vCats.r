3b669a08511f08cbde76ec2da8a1e05ea53aaa87
Adam B. Smith <adam.smith@mobot.org>
2023-11-11 22:08:05 -0600

    Can specify layer

--- R/vCats.r
+++ R/vCats.r
@@ -4,7 +4,9 @@
 #'
 #' @param x A `GVector` or the name of a vector in **GRASS**.
 #'
-#' @param table Logical: If `TRUE`, return category numbers from the data table associated with the vector. If `FALSE` (default), return category numbers from the actual vector.
+#' @param layer Integer, numeric integer, or character: Layer from which to obtain category values.
+#'
+#' @param db Logical: If `TRUE`, return category numbers from the database table associated with the vector. If `FALSE` (default), return category numbers from the actual vector.
 #'
 #' @param integer Logical: If `TRUE` (default), return category values as integers. In some cases, a geometry can have multiple categories, in which case `NA` is returned. If `FALSE`, return category values as strings (and thus, if a geometry has more than one category, does not convert to `NA`).
 #'
@@ -13,7 +15,7 @@
 #' @aliases .vCats
 #' @rdname vCats
 #' @noRd
-.vCats <- function(x, table = FALSE, integer = TRUE) {
+.vCats <- function(x, layer = 1, db = FALSE, integer = TRUE) {
 
 	if (inherits(x, "GVector")) {
 		.restore(x)
@@ -22,10 +24,19 @@
 		src <- x
 	}
 
-	if (table) {
+	layer <- as.character(layer)
+
+	if (db) {
 		out <- .vAsDataTable(src)$cat
 	} else {
-		out <- rgrass::execGRASS("v.category", input = src, option = "print", flags = "quiet", intern = TRUE)
+		out <- rgrass::execGRASS(
+			"v.category",
+			input = src,
+			layer = layer,
+			option = "print",
+			flags = "quiet",
+			intern = TRUE
+		)
 	}
 	if (integer) out <- as.integer(out)
 	out
