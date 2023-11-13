60fc0ab3830f960af920c35c8ad1405df9e11c1e
Adam B. Smith <adam.smith@mobot.org>
2023-11-12 23:07:41 -0600

    Delete !REMOVE THEN REPOP cats.r

--- !REMOVE THEN REPOP cats.r
+++
@@ -1,8 +0,0 @@
-    srcIn <- src
-    src <- .makeSourceName("v_category", "vector")
-    rgrass::execGRASS("v.category", input = srcIn, output = src, option = "del", cat = -1, flags = c("quiet", "overwrite"))
-
-    srcIn <- src
-    src <- .makeSourceName("v_category", "vector")
-    rgrass::execGRASS("v.category", input = srcIn, output = src, option = "add", flags = c("quiet", "overwrite"))
-
