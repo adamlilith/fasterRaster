d4cdb480b8da75388cd2c589ccec50222618f904
Adam B. Smith <adam.smith@mobot.org>
2023-11-19 23:32:43 -0600

    Create vNames.r

---
+++ R/vNames.r
@@ -0,0 +1,31 @@
+#' GRASS vector attribute table column names
+#'
+#' @description This function returns the column names of a **GRASS** vector's attribute table.
+#'
+#' @param x A `GVector` or the name of a vector in **GRASS**.
+#' 
+#' @returns Character vector.
+#' 
+#' @aliases .vNames
+#' @rdname vNames
+#' @noRd
+.vNames <- function(x) {
+
+	if (inherits(x, "GVector")) {
+		.restore(x)
+		src <- sources(x)
+	} else {
+		src <- x
+	}
+
+	args <- list(
+		cmd = "db.columns",
+		table = src,
+		flags = .quiet(),
+		intern = TRUE
+	)
+
+	out <- do.call(rgrass::execGRASS, args = args)
+	out
+
+}
