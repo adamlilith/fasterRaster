bbf7d6af578b778739ad3a4472146bc6f94a6459
Adam B. Smith <adam.smith@mobot.org>
2023-11-20 23:40:15 -0600

    Does not require length(i) to be <= # rasters

--- R/layerIndex.r
+++ R/layerIndex.r
@@ -11,16 +11,18 @@
 #' @noRd
 .layerIndex <- function(layer, x, recycle = TRUE) {
 
-	if (length(layer) > nlyr(x)) stop("Raster only contains ", nlyr(x), " layer(s).")
+	nx <- nlyr(x)
 
 	if (is.character(layer)) {
 		layer <- match(layer, names(x))
 		if (anyNA(layer)) stop("Layer not found.")
 	} else if (is.logical(layer)) {
-		if (recycle && length(layer) < nlyr(x)) layer <- rep(layer, length.out = nlyr(x))
+		if (recycle && length(layer) < nx) layer <- rep(layer, length.out = nx)
 		layer <- which(layer)
+	} else if (inherits(layer, c("numeric", "integer"))) {
+		if (any(layer > nx) | any(layer < 1L)) stop("Index out of bounds.")
 	}
-	if (any(!(layer %in% seq_len(nlyr(x))))) stop("Raster only contains ", nlyr(x), " layer(s).")
+	if (any(!(layer %in% seq_len(nx)))) stop("Raster only contains ", nlyr(x), " layer(s).")
 	layer
 
 }
