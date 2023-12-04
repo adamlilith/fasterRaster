7c04bbdd8f7b94eb0cde3ea6b2a5d4895163fd6a
Adam B. Smith <adam.smith@mobot.org>
2023-12-03 21:26:38 -0600

    Fix bug for multi-layer rasters

--- R/freq.r
+++ R/freq.r
@@ -51,7 +51,7 @@ methods::setMethod(
 
 	nLayers <- length(src)
 
-	if (dtype != "CELL" & bins <= 0) stop("Argument ", sQuote("bins"), " must be a positive integer.")
+	if (any(dtype != "CELL") & bins <= 0) stop("Argument ", sQuote("bins"), " must be a positive integer.")
 	
 	# get values for each raster
 	out <- list()
