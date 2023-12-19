cb67c6f091c0dddb64ded72efff0e22dd69dc173
Adam B. Smith <adam.smith@mobot.org>
2023-12-19 00:35:14 -0600

    Update as.data.frame.r

--- R/as.data.frame.r
+++ R/as.data.frame.r
@@ -1,10 +1,10 @@
 #' Convert GVector to a data frame
 #'
-#' @description Convert a `GVector`'s attribute table to a `data.frame` or `data.table`.
+#' @description Convert a `GVector`'s data table to a `data.frame` or `data.table`.
 #'
 #' @param x A `GVector`.
 #'
-#' @returns A `data.frame` or `NULL` (if the `GRaster` has no attribute table).
+#' @returns A `data.frame` or `NULL` (if the `GRaster` has no data table).
 #' 
 #' @seealso [terra::as.data.frame()], [data.table::as.data.table()]
 #' 
@@ -40,45 +40,3 @@ methods::setMethod(
         }
     } # EOF
 )
-
-# #' @aliases as.data.table
-# #' @rdname as.data.frame
-# #' @exportMethod as.data.table
-# methods::setMethod(
-#     f = "as.data.table",
-#     signature = c(x = "ANY"),
-#     definition = function(x, ...) {
-
-# 	if (inherits(x, "GVector")) {
-
-#         if (nrow(x) > 0L) {
-#             x@table
-#         } else {
-#             NULL
-#         }
-
-# 	} else if (inherits(x, "GRaster")) {
-# 	# if (inherits(x, "GRaster")) {
-	
-# 		stop("Cannot convert a GRaster to a data.table.")
-	
-# 	} else {
-# 		# methods::callNextMethod(x, ...)
-# 		# UseMethod("as.data.table", x, ...)
-# 		data.table::as.data.table(x, ...)
-# 	}
-
-#     } # EOF
-# )
-
-# #' @aliases as.data.table
-# #' @rdname as.data.frame
-# #' @export
-# as.data.table <- function(x, ...) UseMethod("as.data.table", x)
-
-# methods::setMethod(
-# 	f = "as.data.table",
-# 	signature = c(x = "data.frame"),
-# 	function(x, ...) methods::callNextMethod(x, ...)
-# )
-
