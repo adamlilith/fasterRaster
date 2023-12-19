0da4948b2e538fbb8d08962423c1c94fedc3e512
Adam B. Smith <adam.smith@mobot.org>
2023-12-19 00:31:55 -0600

    Update topology.r

--- R/topology.r
+++ R/topology.r
@@ -1,10 +1,12 @@
 #' @title Topology (2- or 3-dimensions) of a GRaster or GVector
 #' @aliases topology
-#' @description Rasters and vectors in **GRASS** can have 2-dimensional or 3-dimensional coordinates. This function returns the dimension of the coordinates of a `GSpatial` object or any that inherits from it (i.e., a `GRaster` or `GVector`).
+#' @description `GRaster`s and `GVector`s can have 2-dimensional or 3-dimensional coordinates. This function returns the dimensions of the object.
 #'
 #' @param x A `GSpatial` object (i.e., a `GRaster` or `GVector`).
 #'
-#' @returns Either `2D` or `3D`.
+#' @returns Either "2D" or "3D".
+#'
+#' @seealso [is.2d()], [is.3d()]
 #'
 #' @example man/examples/ex_GRaster.r
 #'
