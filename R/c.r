7d12d4c511800ac0dbfddfd6f3cc7f3f50fa9bf3
Adam B. Smith <adam.smith@mobot.org>
2023-12-21 23:36:01 -0600

    Renumber cats before concatenating

--- R/c.r
+++ R/c.r
@@ -2,13 +2,13 @@
 #'
 #' @description `GRaster`s can be "stacked" using this function, effectively creating a multi-layered raster. This is different from creating a 3-dimensional raster, though such an effect can be emulated using stacking. `GVector`s can be combined into a single vector.  Stacks can only be created when:
 #' * All objects are the same class (either all `GRaster`s or all `GVector`s).
-#' * All objects are in the same **GRASS** ["location" and "mapset"][tutorial_locations_mapsets].
+#' * All objects have the same coordinate reference system (see crs()).
 #' * For `GRaster`s:
-#'      * Horizontal extents are the same.
-#'      * Horizontal dimensions are the same.
-#'      * The topology (2- or 3-dimensional) must be the same. If 3D, then all rasters must have the same number of depths and vertical extents.
+#'      * Horizontal extents are the same (see [ext()]).
+#'      * Horizontal dimensions are the same (see [res()]).
+#'      * The topology (2- or 3-dimensional) must be the same. If 3D, then all rasters must have the same number of depths and vertical extents (see [topology()]).
 #' * For `GVector`s:
-#'      * The geometry (points, lines, or polygons) must be the same.
+#'      * The geometry (points, lines, or polygons) must be the same. See [geomtype()].
 #'
 #' If features (boundaries, lines, etc.) of `GVector`s that are combined are identical or nearly identical, they can be cleaned using [removeDupNodes()] and [snap()].
 #'
@@ -20,9 +20,9 @@
 #'
 #' @return A `GRaster`.
 #' 
-#' @seealso [add<-], [terra::c()], [terra::add<-]
+#' @seealso [add<-], [terra::c()], \code{\link[terra]{add<-}}
 #'
-#' @example man/examples/ex_GRaster_GVector.r
+#' @example man/examples/ex_c_colbind.r
 #'
 #' @aliases c
 #' @rdname c
@@ -97,89 +97,137 @@ setMethod(
 	.locationRestore(x)
 	dots <- list(...)
 
+	if (length(dots) == 0L) return(x)
+
 	# unlist any lists
-	if (length(dots) > 0L) dots <- omnibus::unlistRecursive(dots)
+	dots <- omnibus::unlistRecursive(dots)
 
 	# comparable?
-	if (length(dots) > 0L) {
-		for (i in seq_along(dots)) compareGeom(x, dots[[i]], geometry = TRUE, topo = TRUE)
-	}
+	for (i in seq_along(dots)) compareGeom(x, dots[[i]], geometry = TRUE, topo = TRUE)
 
-	# increment category numbers
-	input <- rep(NA_character_, length(dots) + 1L)
-	input[1L] <- sources(x) # collects names of all vectors
+	### recat everything
+	# v.patch does not increment cat numbers, so different geometries will have the same cat.
+	# This step increases cat numbers in each subsequent vector so conflicts do not happen.
 
-	cats <- list()
-	for (i in seq_along(dots)) {
-		cats[[i]] <- .vCats(dots[[i]])
-	}
+	src <- sources(x)
+	srcs <- sapply(dots, sources)
 
-	catsSoFar <- sort(unique(.vCats(x)))
-	for (i in seq_along(dots)) {
-		if (any(cats[[i]] %in% catsSoFar)) {
-
-			maxCat <- max(catsSoFar)
-			input[i + 1L] <- .vIncrementCats(dots[[i]], add = maxCat, return = "sources")
-			dotCats <- .vCats(input[i + 1])
-			dotCats <- sort(unique(dotCats))
-			catsSoFar <- c(catsSoFar, dotCats)
-
-		} else {
-			input[i + 1L] <- sources(dots[[i]])
-		}
-	}
-
-	# sources of inputs
-	input <- paste(input, collapse = ",")
-
-	src <- .makeSourceName("v_patch", "vector")
-	rgrass::execGRASS(
-		cmd = "v.patch",
-		input = input,
-		output = src,
-  		flags = c(.quiet(), "overwrite") ### ??? "e"???
-	)
+	cats <- .vCats(src, db = FALSE)
+	topCat <- max(cats)
 	
-	table <- as.data.table(x)
-	if (!is.null(table) && nrow(table) > 0L) {
+	for (i in seq_along(dots)) {
+
+		srcs[i] <- .vIncrementCats(srcs[i], add = topCat)
+		cats <- .vCats(srcs[i], db = FALSE)
+		topCat <- max(cats)
 	
-		xNames <- names(table)
-		xClasses <- sapply(table, class)
-		xNcol <- ncol(table)
+	}
 
-		valid <- TRUE
-		i <- 1L
-		while (valid & i <= length(dots)) {
+	### combine vectors
+	# seems like we can combine at least 11 vectors at a time, but not a lot at a time
+	srcsAtATime <- 10L # number of sources to combine at a time (plus the running `x` source)
 
-			dotTable <- as.data.table(dots[[i]])
+	nSrcs <- length(srcs)
+	sets <- ceiling(nSrcs / srcsAtATime)
+	
+	for (set in seq_len(sets)) {
 
-			if (nrow(dotTable) == 0L) {
-				valid <- FALSE
-			} else {
+		index <- (1L + srcsAtATime * (set - 1L)) : min(nSrcs, set * srcsAtATime)
+		srcIn <- srcs[index]
+		input <- paste(srcIn, collapse = ",")
+		input <- paste0(src, ",", input)
+	
+		src <- .makeSourceName("v_patch", "vector")
+
+		rgrass::execGRASS(
+			cmd = "v.patch",
+			input = input,
+			output = src,
+			flags = c(.quiet(), "overwrite")
+		)
+		
+	}
+
+	# # increment category numbers
+	# input <- rep(NA_character_, length(dots) + 1L)
+	# input[1L] <- sources(x) # collects names of all vectors
+
+	# cats <- list()
+	# for (i in seq_along(dots)) {
+		# cats[[i]] <- .vCats(dots[[i]])
+	# }
+
+	# catsSoFar <- sort(unique(.vCats(x)))
+	# for (i in seq_along(dots)) {
+		# if (any(cats[[i]] %in% catsSoFar)) {
+
+			# maxCat <- max(catsSoFar)
+			# input[i + 1L] <- .vIncrementCats(dots[[i]], add = maxCat, return = "sources")
+			# dotCats <- .vCats(input[i + 1])
+			# dotCats <- sort(unique(dotCats))
+			# catsSoFar <- c(catsSoFar, dotCats)
+
+		# } else {
+			# input[i + 1L] <- sources(dots[[i]])
+		# }
+	# }
+
+	# # sources of inputs
+	# input <- paste(input, collapse = ",")
+
+	# src <- .makeSourceName("v_patch", "vector")
+	# rgrass::execGRASS(
+		# cmd = "v.patch",
+		# input = input,
+		# output = src,
+  		# flags = c(.quiet(), "overwrite") ### ??? "e"???
+	# )
+	
+	table <- list(x@table)
+	tables <- lapply(dots, as.data.table)
+	tables <- c(table, tables)
+	table <- do.call(rbind, tables)
+
+	# table <- as.data.table(x)
+	# if (!is.null(table) && nrow(table) > 0L) {
+	
+		# xNames <- names(table)
+		# xClasses <- sapply(table, class)
+		# xNcol <- ncol(table)
+
+		# valid <- TRUE
+		# i <- 1L
+		# while (valid & i <= length(dots)) {
+
+			# dotTable <- as.data.table(dots[[i]])
+
+			# if (nrow(dotTable) == 0L) {
+				# valid <- FALSE
+			# } else {
 				
-				if (ncol(dotTable) != xNcol) {
-					valid <- FALSE
-				} else {
+				# if (ncol(dotTable) != xNcol) {
+					# valid <- FALSE
+				# } else {
 					
-					dotNames <- names(dotTable)
-					dotClasses <- sapply(dotTable, class)
+					# dotNames <- names(dotTable)
+					# dotClasses <- sapply(dotTable, class)
 
-					if (any(dotNames != xNames) | any(dotClasses != xClasses)) {
-						valid <- FALSE
-					} else {
-						table <- rbind(table, dotTable)
-					}
+					# if (any(dotNames != xNames) | any(dotClasses != xClasses)) {
+						# valid <- FALSE
+					# } else {
+						# table <- rbind(table, dotTable)
+					# }
 
-				}
-			}
+				# }
+			# }
 
-			i <- i + 1L
+			# i <- i + 1L
 
-		} # while valid
+		# } # while valid
 
-		if (!valid) table <- data.table::data.table(NULL)
+		# if (!valid) table <- data.table::data.table(NULL)
 
-	}
+	# }
 	
 	.makeGVector(src, table = table)
 	
