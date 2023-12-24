dc4c34aedb9a034b2d63a9964e35c3a06c0cd801
Adam B. Smith <adam.smith@mobot.org>
2023-12-24 00:40:41 -0600

    Add methods for data.frame, data.table, matrix

--- R/colbind.r
+++ R/colbind.r
@@ -1,9 +1,9 @@
 #' Add columns to the data table of a GVector
 #'
-#' @description `colbind()` is liek [cbind()], excet that it adds columns to the data table of a `GVector`. To combine rows, see [c()].
+#' @description `colbind()` is like [cbind()], except that it adds columns to the data table of a `GVector`. To combine rows, see [c()]. You combine multiple `data.frame`s, `data.table`s, or `matrices`, but you can only combine them with one `GVector` at a time.
 #'
-#' @param x
-#' @param ... `data.frame`s, `data.table`s, and/or `matrices`.
+#' @param x A `GVector`, a `data.frame`, or a `data.table`. If `x` is a `GVector`, then subsequent items must be `data.frame`s, `data.table`s, or `matrices`.
+#' @param ... A `GVector`, or `data.frame`s, `data.table`s, and/or `matrices`. If `x` is a `data.frame`, `data.table`, or `matrix`, then at least one argument in `...` must be a `GVector`.
 #'
 #' @returns A `GVector`.
 #'
@@ -26,6 +26,58 @@ methods::setMethod(
 	if (!inherits(table, "data.table")) table <- data.table::as.data.table(table)
 	x@table <- table
 	x
+	
+	} # EOF
+)
+
+#' @aliases colbind
+#' @rdname colbind
+#' @exportMethod colbind
+methods::setMethod(
+	f = "colbind",
+	signature = c(x = "data.frame"),
+	function(x, ...) {
+
+	x <- data.table::as.data.table(x)
+	colbind(x, ...)
+	
+	} # EOF
+)
+
+#' @aliases colbind
+#' @rdname colbind
+#' @exportMethod colbind
+methods::setMethod(
+	f = "colbind",
+	signature = c(x = "matrix"),
+	function(x, ...) {
+
+	x <- data.table::as.data.table(x)
+	colbind(x, ...)
+	
+	} # EOF
+)
+
+#' @aliases colbind
+#' @rdname colbind
+#' @exportMethod colbind
+methods::setMethod(
+	f = "colbind",
+	signature = c(x = "data.table"),
+	function(x, ...) {
+	
+	dots <- list(...)
+	gv <- sapply(dots, inherits, "GVector")
+	
+	if (sum(gv) > 1L) stop("Can only combine tables with one GVector at a time.")
+	tables <- lapply(dots, data.table::as.data.table)
+	tables <- c(x, tables)
+	
+	table <- do.call(cbind, tables)
+	if (!inherits(table, "data.table")) table <- data.table::as.data.table(table)
+	out <- dots[[which(gv)]]
+	out@table <- table
+	out
 	
 	} # EOF
 )
