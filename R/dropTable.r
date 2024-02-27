66fe001ec3ee6065878b43e136bd00a271e7d6ca
Adam B. Smith <adam.smith@mobot.org>
2024-02-26 20:26:08 -0600

    Create dropTable.r

---
+++ R/dropTable.r
@@ -0,0 +1,24 @@
+#' Remove a data table from a GVector
+#'
+#' @description This function removes a data table associated with a `GVector`.
+#'
+#' @param x A `GVector.
+#'
+#' @returns A `GVector`.
+#'
+#' @example man/examples/ex_GRaster_GVector.r
+#'
+#' @aliases dropTable
+#' @rdName dropTable
+#' @exportMethod dropTable
+methods::setMethod(
+	f = "dropTable",
+	signature = c(x = "GVector"),
+	function(x) {
+	
+	out <- x
+	out@table <- NULL
+	out
+
+	} # EOF
+)
