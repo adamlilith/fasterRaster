49167c99349406b90bdff371513ab63b03cbb8f8
Adam B. Smith <adam.smith@mobot.org>
2024-03-02 01:01:39 -0600

    Create ex_cbind_rbind.r

---
+++ man/examples/ex_cbind_rbind.r
@@ -0,0 +1,27 @@
+if (grassStarted()) {
+
+# Setup
+library(sf)
+
+# Rivers vector
+madRivers <- fastData("madRivers")
+
+# Convert sf to a GVector
+rivers <- fast(madRivers)
+
+# Subset rivers vector
+rivers1 <- rivers[1:2]
+rivers2 <- rivers[10:11]
+
+# Concatenate rivers
+riversCombo <- rbind(rivers1, rivers2)
+riversCombo
+
+# Add columns
+newCol <- data.frame(x = 1:11)
+riversCols <- cbind(rivers, newCol)
+riversCol
+
+as.data.frame(riversCol)
+
+}
