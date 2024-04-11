566ec60cc3c580c2487c3cb4ec4459c03d52c4c3
Adam B. Smith <adam.smith@mobot.org>
2024-04-10 23:50:28 -0500

    Update grassInfo.r

--- R/grassInfo.r
+++ R/grassInfo.r
@@ -4,11 +4,11 @@
 #'
 #' @param x Character: What to return. Any of:
 #' * `"citation"` (default)
+#' * `"copyright"`: Copyright information
 #' * `"version"`: Version number and release year
-#' * `"version"`: Version number as numeric, major and minor only (e.g., 8.3)
-#' * "`copyright`"
+#' * `"versionNumber"`: Version number as numeric, major and minor only (e.g., 8.3)
 #'
-#' Partial matching is used.
+#' Partial matching is used and case is ignored.
 #' 
 #' @return Character.
 #'
