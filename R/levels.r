ca90c44a0183bac3c11f85039f613c0e070d4cd6
Adam B. Smith <adam.smith@mobot.org>
2024-04-13 16:16:00 -0500

    Update levels.r

--- R/levels.r
+++ R/levels.r
@@ -2,17 +2,17 @@
 #'
 #' @description `GRaster`s can represent [categorical data][tutorial_raster_data_types]. Cell values are actually integers, each corresponding to a category, such as "desert" or "wetland." A categorical raster is associated with a table that matches each value to a category name. The table must be `NULL` (i.e., no categories--so not a categorical raster), or have at least two columns. The first column must have integers and represent raster values. One or more subsequent columns must have category labels. The column with these labels is the "active category".
 #'
-#' * `levels()` displays the "levels" table of a raster (just the value and active category columns)
-#' * `cats()` displays the entire "levels" table of a raster
-#' * `levels()<-` (re)assigns the "levels" table to each layer of a raster. Assigning a "levels" table to an integer raster makes it a categorical raster.
-#' * `categories()` (re)assigns the "levels" table to specific layer(s) of a raster.
+#' * `levels()`: Displays the "levels" table of a raster (just the value and active category columns).
+#' * `cats()`: Displays the entire "levels" table of a raster.
+#' * `levels()<-`: (Re)assigns the "levels" table to each layer of a raster. Assigning a "levels" table to an integer raster makes it a categorical raster.
+#' * `categories()`: (Re)assigns the "levels" table to specific layer(s) of a raster.
 #' * For a complete list of functions relevant to categorical rasters, see [raster data types][tutorial_raster_data_types].
 #'
 #' @param x A `GRaster`.
 #'
 #' @param value A `data.frame`, `data.table`, a list of `data.frames` or `data.tables` with one per raster layer, or a categorical `SpatRaster`. The table's first column is the "value" column and must contain numeric values (of class `numeric` or `character`). If a `SpatRaster` is supplied, then its categories will be transferred to the `GRaster`.
 #'
-#' @param layer Numeric integers, logical vector, or character: Layer(s) for which to obtain levels.
+#' @param layer Numeric integers, logical vector, or character: For `cats()` and `categories()`, this specifies the layer(s)for which to obtain level(s).
 #'
 #' @param active An integer or a character: The index or column name of the column used for category labels (the "active column"). Following [terra::activeCat()], the first column of the "levels" table is ignored, so a value of 1 means to use the second column of the table for labels. A value of 2 means to use the third column, and so on.
 #'
@@ -195,8 +195,25 @@ methods::setMethod(
 	signature = c(x = "GRaster", value = "SpatRaster"),
 	function(x, value) {
 
-	value <- levels(value)
+	value <- cats(value)
 	levels(x) <- value
+	x@activeCat <- terra::activeCat(value, layer = layer)
+	x
+
+	} # EOF
+)
+
+#' @aliases levels<-
+#' @rdname levels
+#' @exportMethod levels<-
+methods::setMethod(
+	f = "levels<-",
+	signature = c(x = "GRaster", value = "GRaster"),
+	function(x, value) {
+
+	value <- cats(value)
+	levels(x) <- value
+	x@activeCat <- activeCat(value, layer = layer)
 	x
 
 	} # EOF
@@ -210,7 +227,7 @@ methods::setMethod(
 	signature = c(x = "GRaster", value = "list"),
 	function(x, value) {
 	
-	if (length(value) != nlyr(x)) stop("The number of level tables is not the same as the number of raster layers. \n  If you want to change the level table of select layers, use categories().")
+	if (length(value) != nlyr(x)) stop("The number of level tables is not the same as the number of raster layers.\n  If you want to change the level table of specifc layers, use categories().")
 
 	for (i in seq_along(value)) {
 
