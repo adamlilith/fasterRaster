710b9262fcb0dc056eed2cc48d213d190124ae2f
Adam B. Smith <adam.smith@mobot.org>
2024-04-22 23:27:42 -0500

    Create backdoor.r

---
+++ R/backdoor.r
@@ -0,0 +1,6 @@
+#' Setup fasterRaster for ABS
+#'
+#' This is a secret function for ABS's machine to be used for faster development of **fasterRaster**.
+#'
+#' @noRd
+.backdoor <- function() faster(grassDir = "C:/Program Files/GRASS GIS 8.3")
