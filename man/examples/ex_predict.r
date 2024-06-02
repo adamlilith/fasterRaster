a73399a8c64fde606c55024b51c31a83891d2236
Adam B. Smith <adam.smith@mobot.org>
2024-06-02 17:49:44 -0500

    Update ex_predict.r

--- man/examples/ex_predict.r
+++ man/examples/ex_predict.r
@@ -5,42 +5,49 @@ library(sf)
 library(terra)
 
 ### This example creates a simple model of Dypsis distribution using
-# elevation, distance to forest, and land cover class.
+# elevation, distance to forest, land cover class, and nearness to rivers.
 
 # Elevation raster, forest cover in year 2000, land cover class, and
 # points where Dypsis plants have been collected
 madElev <- fastData("madElev")
 madForest2000 <- fastData("madForest2000")
 madCover <- fastData("madCover")
+madRivers <- fastData("madRivers")
 madDypsis <- fastData("madDypsis")
 
 # Convert SpatRasters to GRasters and sf vector to GVector:
 elev <- fast(madElev)
 forest <- fast(madForest2000)
-dypsis <- fast(madDypsis)
 cover <- fast(madCover)
+rivers <- fast(madRivers)
+dypsis <- fast(madDypsis)
 
 # Distance to forest
-distToForest <- distance(forest, unit = "km")
+distToForest <- distance(forest, unit = "m")
 distToForest <- log1p(distToForest) # log(x + 1) of distance
 names(distToForest) <- "distToForest"
 
 # "Stack" elevation and forest cover
-land <- c(elev, distToForest)
-plot(land)
+continuous <- c(elev, distToForest)
 
 # Scale continuous predictors to mean of 0 and sd of 1
-landScaled <- scale(land)
-names(landScaled) <- c("elevation", "distToForest")
+continuousScaled <- scale(continuous)
+names(continuousScaled) <- c("elevation", "distToForest")
 
 # Project land cover raster
-coverProj <- project(cover, landScaled)
+coverProj <- project(cover, continuousScaled)
+
+# Near a river?
+riverBuffer <- buffer(rivers, 5000)
+nearRiver <- rasterize(riverBuffer, elev, background = 0)
+names(nearRiver) <- "nearRiver"
+levels(nearRiver) <- data.frame(value = 0:1, label = c("far", "near"))
 
 # Combine continuous/categorical data
-covariateRasters <- c(landScaled, coverProj)
+covariateRasters <- c(continuousScaled, coverProj, nearRiver)
+plot(covariateRasters)
 
-### Make model of Dypsis locations:
-# Extract elevation and forest cover at Dypsis locations
+# Extract elevation and forest cover at Dypsis locations:
 presEnv <- extract(covariateRasters, dypsis, cats = TRUE)
 presEnv$presBg <- 1
 head(presEnv)
@@ -58,7 +65,7 @@ env <- rbind(presEnv, bgEnv)
 # Calibrate model:
 form <- presBg ~ elevation + distToForest +
 I(distToForest^2) + elevation * distToForest +
-madCover
+madCover + nearRiver + nearRiver:madCover
 
 model <- stats::glm(form, data = env, family = stats::binomial)
 summary(model)
@@ -67,9 +74,8 @@ summary(model)
 prediction <- predict(covariateRasters, model, type = "response")
 prediction
 
+# Not a great model!
 plot(prediction)
 plot(dypsis, pch = 1, add = TRUE)
-
-# Not a great model!
 
 }
