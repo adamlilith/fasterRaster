f95926504d63de4e4f19cd2d8256601d5ae01968
Adam B. Smith <adam.smith@mobot.org>
2024-06-02 20:14:22 -0500

    `predict()`: Add cat:cat and cat:scalar predictors

--- R/predict.r
+++ R/predict.r
@@ -7,8 +7,9 @@
 #' This `predict()` function can handle:
 #' * Linear predictors and intercepts like `y ~ 1 + x`;
 #' * Quadratic terms like `y ~ x^2` (or, in **R** formular notation, `y ~ I(x^2)`);
-#' * Two-way interaction terms like `y ~ x1:x2` and `y ~ x1 * x2`;
-#' * Categorical predictors (i.e., [categorical][tutorial_raster_data_types] rasters); and
+#' * Two-way interaction terms between scalars like `y ~ x1:x2` and `y ~ x1 * x2`;
+#' * Categorical predictors (i.e., [categorical][tutorial_raster_data_types] rasters);
+#' * Two-way interactions between a categorical predictor and a scalar predictor; and
 #' * Two-way interactions between categorical predictors.
 #'
 #' @param object A `GRaster` with one or more layers.
@@ -59,7 +60,7 @@ methods::setMethod(
 		terms$termClean[i] <- as.character(terms$value[i])
 	}
 
-	# factors... assuming we just use levels of factors as intercepts, not in interaction terms
+	# single factors... assuming we just use levels of factors as intercepts, not in interaction terms
 	if (any(is.factor(object))) {
 
 		facts <- which(is.factor(object))
@@ -90,68 +91,137 @@ methods::setMethod(
 	}
 
 	# interactions between factors
-	if (any(is.factor(object))) {
-		
-		i <- which(is.na(terms$termClean) & grepl(terms$term, pattern = ":"))
-		if (length(i) > 0L) {
+	i <- which(is.na(terms$termClean) & grepl(terms$term, pattern = ":"))
+	if (sum(is.factor(object)) >= 2L & length(i) > 0L) {
 
-			# list contains three items of information:
-			# each element named after the layer's name
-			# each element contains factor values
-			# names of values are the names as they'd appear in coefficients
-			facts <- which(is.factor(object))
-			possibleNames <- list()
-			for (fact in facts) {
-				
-				levs <- levels(object)[[fact]]
-				index <- length(possibleNames) + 1L
-				possibleNames[[index]] <- levs[[1L]]
-				names(possibleNames[[index]]) <- paste0(names(object)[fact], levs[[2L]])
-				names(possibleNames)[index] <- sources(object)[fact]
+		# list contains three items of information:
+		# each element named after the layer's name
+		# each element contains factor values
+		# names of values are the names as they'd appear in coefficients
+		facts <- which(is.factor(object))
+		possibleNames <- list()
+		for (fact in facts) {
+			
+			levs <- levels(object)[[fact]]
+			index <- length(possibleNames) + 1L
+			possibleNames[[index]] <- levs[[1L]]
+			names(possibleNames[[index]]) <- paste0(names(object)[fact], levs[[2L]])
+			names(possibleNames)[index] <- sources(object)[fact]
 
-			}
+		}
 
-			# replace "nice" formula term with if() statement testing equality for each factor level combination in model
-			for (ii in i) { # each row in terms table
-				for (jj in 1L:(length(possibleNames) - 1L)) { # each element in factor list
-					for (jCount in seq_along(possibleNames[[jj]])) { # each item in the element
-						jItem <- possibleNames[[jj]][jCount]
-						jName <- names(jItem)
-						for (kk in (jj + 1L):length(possibleNames)) { # remaining elements in factor list
-							for (kCount in seq_along(possibleNames[[kk]])) { # each item in element
-								kItem <- possibleNames[[kk]][kCount]
-								kName <- names(kItem)
-								if (terms$term[ii] == paste0(jName, ":", kName) | terms$term[ii] == paste0(kName, ":", jName)) {
-									
-									jSrc <- names(possibleNames)[jj]
-									kSrc <- names(possibleNames)[kk]
+		# replace "nice" formula term with if() statement testing equality for each factor level combination in model
+		for (ii in i) { # each row in terms table
+			for (jj in 1L:(length(possibleNames) - 1L)) { # each element in factor list
+				for (jCount in seq_along(possibleNames[[jj]])) { # each item in the element
+					jItem <- possibleNames[[jj]][jCount]
+					jName <- names(jItem)
+					for (kk in (jj + 1L):length(possibleNames)) { # remaining elements in factor list
+						for (kCount in seq_along(possibleNames[[kk]])) { # each item in element
+							kItem <- possibleNames[[kk]][kCount]
+							kName <- names(kItem)
+							if (terms$term[ii] == paste0(jName, ":", kName) | terms$term[ii] == paste0(kName, ":", jName)) {
+								
+								jSrc <- names(possibleNames)[jj]
+								kSrc <- names(possibleNames)[kk]
 
-									# value of coefficient... at least one of these will be empty
-									# both will be empty if there were no cases of crosses between the two factor levels
-									thisCoeff1 <- coeffs[paste0(jName, ":", kName)]
-									thisCoeff2 <- coeffs[paste0(kName, ":", jName)]
-									thisCoeff <- c(thisCoeff1, thisCoeff2)
-									thisCoeff <- thisCoeff[!is.na(thisCoeff)]
+								# value of coefficient... at least one of these will be empty
+								# both will be empty if there were no cases of crosses between the two factor levels
+								thisCoeff1 <- coeffs[paste0(jName, ":", kName)]
+								thisCoeff2 <- coeffs[paste0(kName, ":", jName)]
+								thisCoeff <- c(thisCoeff1, thisCoeff2)
+								thisCoeff <- thisCoeff[!is.na(thisCoeff)]
 
-									if (length(thisCoeff) > 0L) {
+								if (length(thisCoeff) > 0L) {
 
-										terms$termClean[[ii]] <- paste0("if(", jSrc, "==", jItem, " & ", kSrc, "==", kItem, ",", thisCoeff, ",0)")
-									} else {
-										terms$termClean[[ii]] <- NA
-									}
-									
+									terms$termClean[[ii]] <- paste0("if(", jSrc, "==", jItem, " & ", kSrc, "==", kItem, ",", thisCoeff, ",0)")
+								} else {
+									terms$termClean[[ii]] <- NA
 								}
+								
 							}
 						}
 					}
 				}
 			}
-
 		}
 
 	}
 
-	# numeric linear terms
+	# interactions between factors and scalars
+	i <- which(is.na(terms$termClean) & grepl(terms$term, pattern = ":"))
+	if (any(is.factor(object)) & any(!is.factor(object)) & length(i) > 0L) {
+		
+		# list contains three items of information:
+		# each element named after the layer's name
+		# each element contains factor values
+		# names of values are the names as they'd appear in coefficients
+		facts <- which(is.factor(object))
+		possibleNames <- list()
+		for (fact in facts) {
+			
+			levs <- levels(object)[[fact]]
+			index <- length(possibleNames) + 1L
+			possibleNames[[index]] <- levs[[1L]]
+			names(possibleNames[[index]]) <- paste0(names(object)[fact], levs[[2L]])
+			names(possibleNames)[index] <- sources(object)[fact]
+
+		}
+
+		scalarNames <- names(object)[!is.factor(object)]
+		scalarNames <- scalarNames[order(nchar(scalarNames), decreasing = TRUE)]
+		for (ii in i) {
+		
+			term <- terms$term[ii]
+			matchScalar <- FALSE
+			scalar <- 1L
+			while (!matchScalar & scalar <= length(scalarNames)) {
+
+				scalarName <- scalarNames[scalar]
+				if (grepl(term, pattern = scalarName)) {
+
+					matchScalar <- TRUE
+					matchFact <- FALSE
+					fact <- 1L
+					while (!matchFact & fact <= length(possibleNames)) {
+
+						for (j in seq_along(possibleNames[[fact]])) {
+							
+							factNameLevel <- names(possibleNames[[fact]][j])
+							if (grepl(term, pattern = factNameLevel)) {
+								
+								matchFact <- TRUE
+
+								thisCoeff1 <- coeffs[paste0(factNameLevel, ":", scalarName)]
+								thisCoeff2 <- coeffs[paste0(scalarName, ":", factNameLevel)]
+								thisCoeff <- c(thisCoeff1, thisCoeff2)
+								thisCoeff <- thisCoeff[!is.na(thisCoeff)]
+
+								scalarSrc <- sources(object)[[which(names(object) == scalarName)]]
+								factSrc <- names(possibleNames)[fact]
+								levelValue <- possibleNames[[fact]][j]
+
+								terms$termClean[ii] <- paste0(thisCoeff, " * if(", factSrc, "==", levelValue, ",1,0) * ", scalarSrc)
+
+							} # if factor is in term
+						
+						} # next level of factor
+
+						fact <- fact + 1L
+
+					} # next factor
+				
+				} # if scalar is in this term
+			
+				scalar <- scalar + 1L
+
+			} # next scalar
+
+		} # next term
+
+	} # scalar * factor
+
+	# scalar linear terms
 	i <- which(is.na(terms$termClean) & !grepl(terms$term, pattern = ":"))
 	if (length(i) > 0L) {
 	
