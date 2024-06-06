0f884c5a9401fc332f76e84490639036a9db84a9
Adam B. Smith <adam.smith@mobot.org>
2024-06-06 09:49:33 -0500

    Add `byStratum` argument to `GVector` sig

--- R/spatSample.r
+++ R/spatSample.r
@@ -16,6 +16,8 @@
 #'
 #' @param strata Either `NULL` (default), or a `GVector` defining strata. If supplied, the `size` argument will be interpreted as number of points to place per geometry in `strata`. Note that using strata can dramatically slow the process.
 #'
+#' @param byStratum Logical: If `FALSE` (default), then `size` number of points will be placed within the entire area delineated by `strata`. If `TRUE`, then `size` points will be placed within each subgeometry of `strata`.
+#'
 #' @param zlim Either `NULL` (default), or a vector of two numbers defining the lower and upper altitudinal bounds of coordinates. This cannot be combined with `values = TRUE` or `cats = TRUE`.
 #'
 #' @param seed Either `NULL` (default), or an integer. Used as the random seed.
@@ -40,6 +42,7 @@ methods::setMethod(
 		cats = FALSE,
 		xy = FALSE,
 		strata = NULL,
+		byStratum = FALSE,
 		zlim = NULL
 	) {
 
@@ -56,29 +59,58 @@ methods::setMethod(
 	if (is.null(strata)) {
 
 		# for unprojected, we want to adjust for smaller cells near poles, so we oversample, then subsample
+		extent <- ext(x, vector = TRUE)
 		if (is.lonlat(x)) {
-			sizeInflated <- round(2 * size)
+
+			# sampling scheme for latitude:
+			# divide y extent into large number of bins
+			# weight each bin by cos(latitude)
+			# sample with replacement from bin y values
+			# add a uniform random number +- half bin width
+
+			yres <- yres(x)
+			yLower <- extent[3L] + 0.5 * yres
+			yUpper <- extent[4L] - 0.5 * yres
+			ydim <- dim(x)[2L]
+			nMarks <- 100 * ydim
+			halfDelta <- 0.5 * (yUpper - yLower) / (nMarks - 1)
+			yMarks <- seq(yLower, yUpper, length.out = nMarks)
+			w <- abs(cos(pi * yMarks / 180))
+			ys <- sample(yMarks, size, prob = w, replace = TRUE)
+			ys <- ys + runif(size, -halfDelta, halfDelta)
+		
 		} else {
-			sizeInflated <- size
+			# if not long/lat, just sample uniformly
+			ys <- runif(size, extent[3L], extent[4L])
 		}
 
-		extent <- ext(vector = TRUE)
-		xs <- runif(sizeInflated, extent[1L], extent[2L])
-		ys <- runif(sizeInflated, extent[3L], extent[4L])
-		if (!is.null(zlim)) zs <- runif(sizeInflated, zlim[1L], zlim[2L])
+		xs <- runif(size, extent[1L], extent[2L])
+		if (!is.null(zlim)) zs <- runif(siz3, zlim[1L], zlim[2L])
 
-		if (is.lonlat(x)) {
+		# # # # for unprojected, we want to adjust for smaller cells near poles, so we oversample, then subsample
+		# # # if (is.lonlat(x)) {
+		# # # 	sizeInflated <- round(2 * size)
+		# # # } else {
+		# # # 	sizeInflated <- size
+		# # # }
+
+		# # # extent <- ext(vector = TRUE)
+		# # # xs <- runif(sizeInflated, extent[1L], extent[2L])
+		# # # ys <- runif(sizeInflated, extent[3L], extent[4L])
+		# # # if (!is.null(zlim)) zs <- runif(sizeInflated, zlim[1L], zlim[2L])
+
+		# # # if (is.lonlat(x)) {
 		
-			w <- abs(cos(pi * ys / 180))
-			w <- size / sum(w) * w
-			keeps <- sampling::UPrandomsystematic(w)
-			keeps <- which(keeps == 1)
-			if (length(keeps) != size) stop("Did not select desired number.")
-			xs <- xs[keeps]
-			ys <- ys[keeps]
-			if (!is.null(zlim)) zs <- zs[keeps]
+		# # # 	w <- abs(cos(pi * ys / 180))
+		# # # 	w <- size / sum(w) * w
+		# # # 	keeps <- sampling::UPrandomsystematic(w)
+		# # # 	keeps <- which(keeps == 1)
+		# # # 	if (length(keeps) != size) stop("Did not select desired number.")
+		# # # 	xs <- xs[keeps]
+		# # # 	ys <- ys[keeps]
+		# # # 	if (!is.null(zlim)) zs <- zs[keeps]
 		
-		}
+		# # # }
 
 		if (xy) {
 			if (is.null(zlim)) {
@@ -106,7 +138,7 @@ methods::setMethod(
 			output = src,
 			format = "point",
 			separator = "pipe",
-			flags = c(.quiet(), "verbose", "overwrite", "t")
+			flags = c(.quiet(), "verbose", "overwrite", "t", "n")
 		)
 		if (!is.null(zlim)) args$flags <- c(args$flags, "z")	
 		do.call(rgrass::execGRASS, args = args)
@@ -114,6 +146,8 @@ methods::setMethod(
 	# slow point location... use GRASS
 	} else {
 		
+		if (size > 250000) .message("spatSample_strata", "Using `strata` when selecting a large number of points can take a long time.")
+
 		args <- list(
 			cmd = "v.random",
 			output = src,
@@ -123,7 +157,7 @@ methods::setMethod(
 
 		if (!is.null(strata)) {
 			args$restrict <- sources(strata)
-			args$flags <- c(args$flags, "a")
+			if (byStratum) args$flags <- c(args$flags, "a")
 		}
 
 		if (!is.null(zlim)) {
@@ -132,7 +166,8 @@ methods::setMethod(
 			args$flags <- c(args$flags, "z")
 		}
 
-		if (!is.null(seed)) args$seed <- seed
+		# if (!is.null(seed)) args$seed <- seed
+		args$seed <- round(1E9 * runif(1))
 
 		# args$flags <- c(args$flags, "b") ### do not create topology... problems? YES!
 		do.call(rgrass::execGRASS, args = args)
@@ -227,7 +262,7 @@ methods::setMethod(
 		as.points = FALSE,
 		values = TRUE,
 		xy = FALSE,
-		strata = NULL,
+		byStratum = FALSE,
 		zlim = NULL,
 		seed = NULL
 	) {
@@ -237,30 +272,30 @@ methods::setMethod(
 
 	if (geomtype(x) != "polygons") x <- convHull(x)
 
-	# if sampling by strata, keep polygons as-is
-	if (!is.null(strata)) {
+	# # if sampling by strata, keep polygons as-is
+	# if (!is.null(strata)) {
 
-		srcRestrict <- sources(x)
+	# 	srcRestrict <- sources(x)
 
-	# if not sampling by strata, dissolve all polygons
-	} else {
+	# # if not sampling by strata, dissolve all polygons
+	# } else {
 
-		# make copy of vector and force all categories to 1
-		gtype <- geomtype(x)
-		srcRestrict <- .aggregate(sources(x), dissolve = TRUE, gtype = gtype, copy = TRUE)
+	# 	# make copy of vector and force all categories to 1
+	# 	gtype <- geomtype(x)
+	# 	srcRestrict <- .aggregate(sources(x), dissolve = TRUE, gtype = gtype, copy = TRUE)
 
-	}
+	# }
 	
-	src <- .makeSourceName("v_random", "vector")
+	src <- .makeSourceName("spatSample_v_random", "vector")
 	args <- list(
 		cmd = "v.random",
 		output = src,
 		npoints = size,
-		restrict = srcRestrict,
+		restrict = src,
 		flags = c(.quiet(), "overwrite")
 	)
 
-	if (!is.null(strata)) args$flags <- c(args$flags, "a")
+	if (!is.null(strata) & byStratum) args$flags <- c(args$flags, "a")
 
 	if (!is.null(zlim)) {
 		args$zmin <- zlim[1L]
