e2e7b12e83786d4f1116b2384062555e31cbf987
Adam B. Smith <adam.smith@mobot.org>
2024-08-23 22:21:06 -0500

    Rework addTable()/dropTable()/colbind()/rbind() example

--- man/examples/ex_cbind_rbind_addTable_dropTable.r
+++ man/examples/ex_cbind_rbind_addTable_dropTable.r
@@ -9,6 +9,10 @@ madRivers <- fastData("madRivers")
 # Convert sf to a GVector
 rivers <- fast(madRivers)
 
+# Convert GVector to data.frame or data.table
+as.data.frame(rivers)
+as.data.table(rivers)
+
 # Subset rivers vector
 rivers1 <- rivers[1:2]
 rivers2 <- rivers[10:11]
@@ -22,17 +26,15 @@ newCol <- data.frame(new = 1:11)
 riversCol <- colbind(rivers, newCol)
 riversCol
 
-as.data.frame(riversCol)
-
 # Remove table
 riversCopy <- rivers
-riversCopy # 11 rows in data table
-table(riversCopy) <- NULL
+riversCopy # has data table
+riversCopy <- dropTable(riversCopy)
 riversCopy # no data table
 
 # Add a new table
 newTable <- data.frame(num = 1:11, letters = letters[1:11])
-table(riversCopy) <- newTable
+addTable(riversCopy) <- newTable
 riversCopy
 
 }
