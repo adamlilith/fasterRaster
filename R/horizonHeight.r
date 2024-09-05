ae002bae1fded30c9e97fdbaaa3ea6675a58a96d
Adam B. Smith <adam.smith@mobot.org>
2024-09-04 23:39:07 -0500

    Change `directions` to `step` (code-breaking!)

--- R/horizonHeight.r
+++ R/horizonHeight.r
@@ -6,9 +6,9 @@
 #'
 #' @param units Character: Units of the height. Either `radians` (default) or `degrees`. Partial matching is used.
 #'
-#' @param directions Numeric vector with positive integers between 0 and 359, inclusive: Direction(s) in which to calculate horizon height for each cell. By default, these are given in degrees clockwise from 0, so 0 is north, 90 east, 180 south, and 270 west. However, if you set `northIs0 = FALSE`, then the directions are given degrees counterclockwise from east, so east is 0, north 90, west 180, and south 270. Regardless, the default is to calculate horizon angle in all four directions. One raster is created per direction. Note that the output will be labeled according to the angle of the directions (e.g., `horizonHeight_090` will be horizon height facing east if `northIs0 = TRUE` (default), but horizon height facing north if `northIs0 = FALSE`. Note that **GRASS** automatically rounds these values down to the nearest integer, so this function does the same but also produces a warning.
+#' @param step Numeric integer between 0 and 360, inclusive: Angle step size (in degrees) for calculating horizon height. The direction in which horizon height is calculated is incremented from 0 to 360, with the last value excluded.
 #'
-#' @param northIs0 Logical: If `TRUE` (default), argument `directions` specifies horizon height in "north orientation," or clockwise from 0, so 0 is north, 90 east, 180 south, and 270 west. If `FALSE`, angles are in "east orientation", or counterclockwise from east, so east is 0, north 90, west 180, and south 270. North orientation is the default for this function in **R**, but east orientation is the default in the **GRASS** module `r.horizon`. **Note:** The [sun()] function requires aspect to be in east orientation.
+#' @param northIs0 Logical: If `TRUE` (default), horizon height calculated in the 0-degree direction will be facing north,  and proceed clockwise So, under "north orientation", 0 is north, 90 east, 180 south, and 270 west. If `FALSE`, angles are in "east orientation", and proceed counterclockwise from east. So, east is 0, north 90, west 180, and south 270. North orientation is the default for this function in **R**, but east orientation is the default in the **GRASS** module `r.horizon`. **Note:** The [sun()] function requires aspect to be in east orientation.
 #'
 #' @param bufferZone Numeric >= 0 (default is 0): A buffer of the specified width will be generated around the raster before calculation of horizon angle. If the coordinate system is in longitude/latitude (e.g., WGS84 or NAD83), then this is specified in degrees. Otherwise units are map units (usually meters).
 #'
@@ -31,7 +31,7 @@ methods::setMethod(
 	definition = function(
 		x,
 		units = "radians",
-		directions = c(0, 90, 180, 270),
+		step = 90,
 		northIs0 = TRUE,
 		bufferZone = 0,
 		distance = 1,
@@ -39,47 +39,47 @@ methods::setMethod(
 	) {
 	
 	units <- omnibus::pmatchSafe(units, c("radians", "degrees"))
-	if (any(!omnibus::is.wholeNumber(directions))) {
-		directions <- floor(directions)
-		warning("Non-integer value(s) used for argument ", sQuote("directions"), ".\n  Value(s) rounded down (GRASS will do this anyway).")
+	
+	if (any(!omnibus::is.wholeNumber(step))) {
+		step <- round(step)
+		warning("Non-integer value used for argument `step`.\n  Value will be rounded (GRASS will do this anyway).")
 	}
 	
-	if (any(directions < 0 | directions >= 360)) stop("Values in argument ", sQuote("directions"), " must be in the range [0, 359].")
+	if (step < 0 | step >= 360) stop("Argument `step` must be in the range [0, 359].")
 	
-	flags <- c(.quiet(), "overwrite")
-	if (northIs0) flags <- c(flags, "c")
-	if (units == "degrees") flags <- c(flags, "d")
-	
+	src <- .makeSourceName("horizonHeight", "raster")
 	args <- list(
 		cmd = "r.horizon",
 		elevation = sources(x),
-		output = NA_character_,
-		direction = NA_real_,
+		output = src,
+		step = step,
+		start = 0,
+		end = 360,
 		bufferzone = bufferZone,
 		distance = distance,
-		flags = flags
+		flags = c(.quiet(), "overwrite")
 	)
 	
-	if (!is.null(maxDist)) args$maxdistance <- maxDist
+	# The "c" flag does not seem to work as described in `r.horizon` when using `start`, `end`, and `step` arguments in the call to `r.horizon`. So, we'll calculate horizon height assuming east orientation and if needed, sort the rasters so they match north orientation.
+	# if (northIs0) args$flags <- c(args$flags, "c")
 	
+	if (units == "degrees") args$flags <- c(args$flags, "d")
+	if (!is.null(maxDist)) args$maxdistance <- maxDist
+
 	.locationRestore(x)
 	.region(x)
-	
-	nDirections <- length(directions)
 
-	srcs <- .makeSourceName("horizon", "rast", n = nDirections)
-	for (i in seq_len(nDirections)) {
-	
-		direction <- directions[i]
-		niceDirection <- sprintf("%03.0f", direction)
-		args$direction <- direction
-		args$output <- srcs[i]
-	
-		do.call(rgrass::execGRASS, args = args)
-	
-	}
+	do.call(rgrass::execGRASS, args = args)
+
+	directions <- seq(0, 359.9999999, by = step)
+	if (northIs0) directions <- reorient(directions)
+
 	niceDirections <- sprintf("%03.0f", directions)
-	.makeGRaster(paste0(srcs, "_", niceDirections), names = paste0("horizonHeight_", niceDirections, "deg"))
+	srcs <- paste0(src, "_", niceDirections)
+	
+	if (northIs0) niceDirections <- sort(niceDirections)
+
+	.makeGRaster(srcs, names = paste0("horizonHeight_", niceDirections, "deg"))
 
 	} # EOF
 )
