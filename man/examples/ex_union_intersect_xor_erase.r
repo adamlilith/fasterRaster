07c58b74b2626020573b7a25c47693ee1f18c2e3
Adam B. Smith <adam.smith@mobot.org>
2024-09-05 19:02:08 -0500

    Rename file

---
+++ man/examples/ex_union_intersect_xor_erase.r
@@ -0,0 +1,49 @@
+if (grassStarted()) {
+
+# Setup
+library(sf)
+
+# Polygon of coastal Madagascar and Dypsis specimens
+madCoast4 <- fastData("madCoast4") # polygons
+madDypsis <- fastData("madDypsis") # points
+
+# Convert vectors:
+coast4 <- fast(madCoast4)
+dypsis <- fast(madDypsis)
+
+# Create another polygons vector from a convex hull around Dypsis points
+hull <- convHull(dypsis)
+
+### union()
+###########
+
+unioned <- union(coast4, hull)
+plot(unioned)
+
+plus <- coast4 + hull # same as union()
+
+### intersect
+#############
+
+inter <- intersect(coast4, hull)
+plot(coast4)
+plot(hull, border = "red", add = TRUE)
+plot(inter, border = "blue", add = TRUE)
+
+### xor
+#######
+
+xr <- xor(coast4, hull)
+plot(coast4)
+plot(xr, border = "blue", add = TRUE)
+
+### erase
+#########
+
+erased <- erase(coast4, hull)
+plot(coast4)
+plot(erased, border = "blue", add = TRUE)
+
+minus <- coast4 - hull # same as erase()
+
+}
