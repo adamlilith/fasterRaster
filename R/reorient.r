b8aca819541b5a7b3b5cb1f68df14f90a6ed1d8a
Adam B. Smith <adam.smith@mobot.org>
2024-09-07 19:23:47 -0500

    Works for `units = 'radians'`

--- R/reorient.r
+++ R/reorient.r
@@ -6,6 +6,8 @@
 #'
 #' @param x A numeric vector or a `GRaster` with cell values equal to facing (in degrees).
 #'
+#' @param units Character: "Units" of values in `x`: either `"degrees"` for degrees (default) or `"radians"`. Partial matching is used.
+#'
 #' @returns A `GRaster` or numeric vector. Values will be in the range between 0 and 360 and represents facing in the system "opposing" the input's system. For example, if the input is north orientation, the output will be east orientation. If the input is in east orientation, the output will be in north orientation.
 #'
 #' @example man/examples/ex_reorient.r
@@ -16,8 +18,10 @@
 methods::setMethod(
 	f = "reorient",
 	signature = c(x = "GRaster"),
-	definition = function(x) {
-	
+	definition = function(x, units = "degrees") {
+
+	units <- omnibus::pmatchSafe(units, c("degrees", "radians"), n = 1L)
+
 	.locationRestore(x)
 	.region(x)
 
@@ -26,7 +30,11 @@ methods::setMethod(
 
 	for (i in seq_len(nLayers)) {
 
-		ex <- paste0(srcs[i], " = ((360 - ", sources(x)[i], "% 360) % 360 + 90) % 360")
+		if (units == "degrees") {
+			ex <- paste0(srcs[i], " = ((360 - (", sources(x)[i], " % 360)) + 90) % 360")
+		} else if (units == "radians") {
+			ex <- paste0(srcs[i], " = ((2 * ", pi, " - (", sources(x)[i], " % (2 * ", pi, "))) + (", pi, " / 4)) % (2 * ", pi, ")")
+		}
 		rgrass::execGRASS("r.mapcalc", expression = ex, flags = c(.quiet(), "overwrite"))
 
 	}
@@ -41,5 +49,15 @@ methods::setMethod(
 methods::setMethod(
 	f = "reorient",
 	signature = c(x = "numeric"),
-	definition = function(x) ((360 - (x %% 360)) + 90) %% 360
+	definition = function(x, units = "degrees") {
+	
+	units <- omnibus::pmatchSafe(units, c("degrees", "radians"), n = 1L)
+
+	if (units == "degrees") {
+		((360 - (x %% 360)) + 90) %% 360
+	} else if (units == "radians") {
+		((2 * pi - (x %% (2 * pi))) + (pi / 4)) %% (2 * pi)
+	}
+	
+	} # EOF
 )
