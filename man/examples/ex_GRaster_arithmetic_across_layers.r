326bc558b8839400cd8e4552e2cedd84767fc660
Adam B. Smith <adam.smith@mobot.org>
2024-10-03 23:49:53 -0500

    Recover from GHD BSOD crash

---
+++ man/examples/ex_GRaster_arithmetic_across_layers.r
@@ -0,0 +1,61 @@
+if (grassStarted()) {
+
+# Setup
+library(sf)
+library(terra)
+
+# Example data
+madChelsa <- fastData("madChelsa")
+
+# Convert a SpatRaster to a GRaster
+chelsa <- fast(madChelsa)
+chelsa # 4 layers
+
+# Central tendency
+mean(chelsa)
+mmode(chelsa)
+median(chelsa)
+
+# Statistics
+nunique(chelsa)
+sum(chelsa)
+count(chelsa)
+min(chelsa)
+max(chelsa)
+range(chelsa)
+skewness(chelsa)
+kurtosis(chelsa)
+
+stdev(chelsa)
+stdev(chelsa, pop = FALSE)
+var(chelsa)
+varpop(chelsa)
+
+# Which layers have maximum/minimum?
+which.min(chelsa)
+which.max(chelsa)
+
+# Regression
+slope(chelsa)
+intercept(chelsa)
+r2(chelsa)
+tvalue(chelsa)
+
+# Note: To get quantiles for each layer, use
+# global(x, "quantile", probs = 0.2).
+quantile(chelsa, 0.1)
+
+# NAs
+madForest2000 <- fastData("madForest2000")
+forest2000 <- fast(madForest2000)
+forest2000 <- project(forest2000, chelsa, method = "near")
+
+chelsaForest <- c(chelsa, forest2000)
+
+nas <- anyNA(chelsaForest)
+plot(nas)
+
+allNas <- allNA(chelsaForest)
+plot(allNas)
+
+}
---
+++ man/examples/ex_init.r
@@ -0,0 +1,51 @@
+if (grassStarted()) {
+
+# Setup
+library(terra)
+
+# Elevation raster, rivers vector
+madElev <- fastData("madElev")
+
+# Convert to a GRaster
+elev <- fast(madElev)
+
+# Cell coordinates
+init(elev, "x")
+init(elev, "y")
+
+# Cell row or column
+init(elev, "row")
+init(elev, "col")
+
+# Chess
+elevAgg <- aggregate(elev, 32) # make cells bigger so we can see
+
+chessOdd <- init(elevAgg, "chess")
+chessEven <- init(elevAgg, "chess", odd = FALSE)
+
+chess <- c(chessOdd, chessEven)
+names(chess) <- c("odd", "even")
+plot(chess)
+
+# Chess with user-defined values
+elevAgg <- aggregate(elev, 32) # make cells bigger so we can see
+
+chessOdd13 <- init(elevAgg, "chess", vals = c(0, 13))
+chessEven13 <- init(elevAgg, "chess", odd = FALSE, vals = c(0, 13))
+
+chess13 <- c(chessOdd13, chessEven13)
+names(chess13) <- c("odd", "even")
+plot(chess13)
+
+# Regular
+elevAgg <- aggregate(elev, 32) # make cells bigger so we can see
+
+regOdd <- init(elevAgg, "regular")
+regEven <- init(elevAgg, "regular", odd = FALSE)
+
+reg <- c(regOdd, regEven)
+names(reg) <- c("odd", "even")
+plot(reg)
+
+
+}
