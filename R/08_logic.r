a923f6dac3e0f23ff965741bae26a801213e41cf
Adam B. Smith <adam.smith@mobot.org>
2024-10-03 23:45:16 -0500

    Update 08_logic.r

--- R/08_logic.r
+++ R/08_logic.r
@@ -1,6 +1,6 @@
 #' Logic-methods operations on GRasters
 #'
-#' @description You can do logical operations on `GRaster`s. Here, a value of 1 is interpreted as `TRUE`, and a value of 0 is interpreted as `FALSE`. You can compare:
+#' @description You can do logical operations on `GRaster`s. A cell with a value of 1 is interpreted as `TRUE`, and a value of 0 is interpreted as `FALSE`. You can compare:
 #' * A `GRaster` to another `GRaster`
 #' * A `GRaster` to a logical value (`TRUE` or `FALSE`, but not `NA`--see [not.na()])
 #' * A `GRaster` to a numeric or integer value that is 0 or 1
