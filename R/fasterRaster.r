4fbfbdb0ed0e52cb6c79a0e7f6a92fbf94b4aa3d
Adam B. Smith <adam.smith@mobot.org>
2024-10-06 00:56:48 -0500

    Update fasterRaster.r

--- R/fasterRaster.r
+++ R/fasterRaster.r
@@ -3,9 +3,9 @@
 #' @description **fasterRaster**: Processing of large-in-memory/-on disk rasters and spatial vectors in using **GRASS GIS**. Most functions in the **terra** and **sf** packages are recreated. Processing of medium-sized and smaller spatial objects will nearly always be faster using **terra** or **sf**. To use most of the functions you must have the stand-alone version of **GRASS GIS** version 8.3 or higher (not the **OSGeoW4** installer version). Note that due to differences in how **GRASS**, **terra**, and **sf** were implemented, results will not always be strictly comparable between functions for the same operation.
 #'
 #' ## Most useful tutorials and functions:
-#' * The quick-start guide to getting started with **fasterRaster** , accessible using `vignette("fasterRaster", package = "fasterRaster")`
-#' * The vignette on types of `GRaster`s, accessible using `vignette("GRasters", package = "fasterRaster")`
-#' * The vignette on how to speed up **fasterRaster**, accessible using `vignette("faster_fasterRaster", package = "fasterRaster")`
+#' * The quick-start guide to getting started with **fasterRaster**, accessible using `vignette("fasterRaster", package = "fasterRaster")`
+#' * Types of `GRaster`s, accessible using `vignette("GRasters", package = "fasterRaster")`
+#' * How to speed up **fasterRaster**, accessible using `vignette("faster_fasterRaster", package = "fasterRaster")`
 #' * [faster()]: Set the directory where **GRASS** is installed on your system, and set or get other package-wide options. This function must be run once before using most **fasterRaster** functions.
 #' * [fast()]: Convert a `SpatRaster`, `SpatVector`, or `sf` vector to **fasterRaster**'s raster format (`GRaster`s) or vector format (`GVector`s), or load one from a file
 #' * [rast()], [vect()], and [st_as_sf()]: Convert `GRaster`s and `GVector`s to `SpatRaster`s, `SpatVector`s, or `sf` vectors
