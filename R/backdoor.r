7848c42d656f02b496bb193f6fd1c44ea563955e
Adam B. Smith <adam.smith@mobot.org>
2024-10-09 01:01:45 -0500

    Add juice

--- R/backdoor.r
+++ R/backdoor.r
@@ -1,6 +1,21 @@
 #' Setup fasterRaster for ABS
 #'
-#' This is a secret function for ABS's machine to be used for faster development of **fasterRaster**.
+#' This is a secret function to be used for faster development of **fasterRaster**. It assume development is on a Windows machine.
 #'
-#' @noRd
-.backdoor <- function() faster(grassDir = "C:/Program Files/GRASS GIS 8.4", memory = 1024 * 8, cores = 2, verbose = TRUE)
+#' @param ver Character: **GRASS**: e.g., "83" or "84".
+#'
+#' @returns `TRUE` (invisibly).
+#'
+#' @keywords internal
+.backdoor <- function(ver = "84") {
+	
+	verNice <- paste0(substr(ver, 1L, 1L), ".", substr(ver, 2L, 2L))
+	
+	faster(
+		grassDir = paste0("C:/Program Files/GRASS GIS ", verNice),
+		memory = 1024 * 8,
+		cores = 2,
+		verbose = TRUE
+	)
+	invisible(TRUE)
+}
