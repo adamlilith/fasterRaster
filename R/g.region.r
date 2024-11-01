c1310421fe9d021a314ebad3ffa7b6d4e659c93f
Adam B. Smith <adam.smith@mobot.org>
2024-10-31 23:47:37 -0500

    Create g.region.r

---
+++ R/g.region.r
@@ -0,0 +1,16 @@
+#' Call GRASS `g.region` module
+#'
+#' This function calls the **GRASS** module `g.region` to display information on the region of the current **GRASS** "project".
+#'
+#' @returns Displays current region information for the active "project/location" in **GRASS**.
+#'
+#' @examples
+#'
+#' .g.region()
+#'
+#' @export
+#' @keywords internal
+.g.region <- function() {
+	out <- rgrass::execGRASS("g.region", flags = "p", intern = TRUE)
+	out
+}
