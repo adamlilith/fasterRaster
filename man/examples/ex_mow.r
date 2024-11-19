cd0e371cf2ca749988e302b2f54fb12fb4a08a0e
Adam B. Smith <adam.smith@mobot.org>
2024-11-18 23:32:27 -0600

    Update ex_mow.r

--- man/examples/ex_mow.r
+++ man/examples/ex_mow.r
@@ -1,5 +1,14 @@
 if (grassStarted()) {
 
-if (FALSE) mow()
+# Setup
+madElev <- fastData("madElev")
+elev <- fast(madElev)
+
+# delete GRASS raster attached to `elev`
+if (FALSE) {
+	mow(elev)
+	elev # useless R object still exists, though
+	elev * 2 # will fail because raster has been deleted from GRASS
+}
 
 }
