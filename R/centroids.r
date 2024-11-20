41b6958dca302b021c9ba38bafdbb037d3c8902d
Adam B. Smith <adam.smith@mobot.org>
2024-11-20 00:30:53 -0600

    Create `centroids()`

---
+++ R/centroids.r
@@ -0,0 +1,89 @@
+#' Centroid(s) of a vector
+#'
+#' @description This function locates the centroid of each geometry of a `GVector`. **To use this function, you must a) have correctly specified the `addonsDir` option using [faster()], and b) installed the **GRASS** addon `v.centerpoints`.** See `vignette("addons", package = "fasterRaster")`.
+#'
+#' @param x A `GVector`.
+#' @param method Character or `NULL` (default): Method used for calculating centroids. The method of calculation depends on whether the input is a `points`, `lines`, or `polygons` `GVector`. If the value is `NULL`, then the default method will be chosen, depending on the geometry type of the `GVector`:
+#' * `points`:
+#'    * `"mean"` (default for `points`): Mean of coordinates.
+#'    * `"median"`: Geometric median; more robust to outliers.
+#'    * `"pmedian"`: Point in `x` closest to the geometric median.
+#' * `lines`:
+#'    * `"mid"` (default for `lines`): Mid-point on each line; will fall exactly on the line.
+#'    * `"mean"`: Center of gravity of all line segments; may not fall on the line.
+#'    * `"median`: Geometric median; may not fall on the line.
+#' * `polygons`:
+#'    * `"mean"` (default): Center of gravity (area), calculated using area triangulation.
+#'    * `"median"`: Geometric mean; may not fall inside the polygon.
+#'    * `"bmedian"`: Geometric mean; minimum distance to boundaries; may not fall inside the polygon.
+#' 
+#' Partial matching is used and case is ignored.
+#'
+#' @returns A points `GVector`.
+#'
+#' @example man/examples/ex_centroids.r
+#' 
+#' @seealso [terra::centroids()]; **GRASS** addon module `v.centerpoint`.
+#'
+#' @aliases centroids
+#' @rdname centroids
+#' @exportMethod centroids
+methods::setMethod(
+	f = "centroids",
+	signature = c(x = "GVector"),
+	function(x, method = NULL) {
+	
+	addons("v.centerpoint", verbose = FALSE)
+
+	gtype <- geomtype(x)
+	
+	if (is.null(method)) {
+	
+		if (gtype == "points") {
+			method <- "mean"
+		} else if (gtype == "lines") {
+			method <- "mid"
+		} else if (gtype == "polygons") {
+			method <- "mean"
+		}
+	
+	}
+
+	if (gtype == "points") {
+		methods <- c("mean", "median", "pmedian")
+	} else if (gtype == "lines") {
+		methods <- c("mid", "mean", "median")
+	} else if (gtype == "polygons") {
+		methods <- c("mean", "median", "bmedian")
+	}
+
+	method <- omnibus::pmatchSafe(method, methods, nmax = 1L)
+
+	src <- .makeSourceName("centroids", "vector")
+	args <- list(
+		cmd = "v.centerpoint",
+		input = sources(x),
+		output = src,
+		flags = c(.quiet(), "overwrite")
+	)
+	
+	if (gtype == "points") {
+		args$pcenter <- method
+	} else if (gtype == "lines") {
+		args$lcenter <- method
+	} else if (gtype == "polygons") {
+		args$acenter <- method
+	}
+
+	do.call(rgrass::execGRASS, args = args)
+
+	if (gtype == "points") {
+		table <- NULL
+	} else {
+		table <- as.data.table(x)
+	}
+
+	.makeGVector(src, table = table)
+	
+	} # EOF
+)
---
+++ man/examples/ex_centroids.r
@@ -0,0 +1,66 @@
+if (grassStarted()) {
+
+# Setup
+library(sf)
+library(terra)
+
+# Points, lines, and polygons
+madDypsis <- fastData("madDypsis")
+madRivers <- fastData("madRivers")
+madCoast4 <- fastData("madCoast4")
+
+# Convert to  GVectors:
+dypsis <- fast(madDypsis)
+rivers <- fast(madRivers)
+coast4 <- fast(madCoast4)
+
+# Point centroids:
+dypMean <- centroids(dypsis)
+dypMedian <- centroids(dypsis, method = "median")
+dypPMedian <- centroids(dypsis, method = "pmedian")
+
+plot(dypsis)
+plot(dypMean, col = "red", add = TRUE)
+plot(dypMedian, col = "green", pch = 2, add = TRUE)
+plot(dypPMedian, col = "orange", pch = 1, add = TRUE)
+legend("bottomright",
+   legend = c("mean", "median", "pmedian"),
+   col = c("red", "green", "orange"),
+   pch = c(16, 2, 1),
+   xpd = NA
+)
+
+# Line centroids:
+riversMid <- centroids(rivers)
+riversMean <- centroids(rivers, method = "mean")
+riversMedian <- centroids(rivers, method = "median")
+
+plot(rivers)
+plot(riversMid, col = "red", add = TRUE)
+plot(riversMean, col = "green", pch = 2, add = TRUE)
+plot(riversMedian, col = "orange", pch = 1, add = TRUE)
+legend("bottomright",
+   legend = c("mid", "mean", "median"),
+   col = c("red", "green", "orange"),
+   pch = c(16, 2, 1),
+   xpd = NA
+)
+
+
+# Polygon centroids:
+coast4Mean <- centroids(coast4)
+coast4Median <- centroids(coast4, method = "median")
+coast4BMedian <- centroids(coast4, method = "bmedian")
+
+plot(coast4)
+plot(coast4Mean, col = "red", add = TRUE)
+plot(coast4Median, col = "green", pch = 2, add = TRUE)
+plot(coast4Median, col = "orange", pch = 1, add = TRUE)
+legend("bottomright",
+   legend = c("mean", "median", "bmedian"),
+   col = c("red", "green", "orange"),
+   pch = c(16, 2, 1),
+   xpd = NA
+)
+
+}
