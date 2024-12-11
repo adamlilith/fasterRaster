dd39f6f741c86143c92203e9bffa0562e15e1788
Adam B. Smith <adam.smith@mobot.org>
2024-12-11 11:17:03 -0600

    Bug fix: If `x` has no `db`, add one

--- R/crds.r
+++ R/crds.r
@@ -58,14 +58,20 @@ methods::setMethod(
 methods::setMethod(
 	f = "crds",
 	signature = c(x = "GVector"),
-	function(x, z = is.3d(x)) .crdsVect(x, z = z)
+	function(x, z = is.3d(x)) {
+
+	cats <- if (geomtype(x) == "points") { 1:ngeom(x) } else { NULL }
+	.crdsVect(x, z = z, cats = cats)
+	
+	} # EOF
 )
 
 #' @rdname crds
 #' @export
 st_coordinates <- function(x) {
-	if (inherits(x, "GSpatial")) {
-		.crdsVect(x, z = FALSE)
+	if (inherits(x, "GVector")) {
+		cats <- if (geomtype(x) == "points") { 1:ngeom(x) } else { NULL }
+		.crdsVect(x, z = FALSE, cats = cats)
 	} else {
 		sf::st_coordinates(x)
 	}
@@ -76,14 +82,16 @@ st_coordinates <- function(x) {
 #' @param x A GVector or [sources()] name of one.
 #' @param z T/F Extract z-coordinate?
 #' @param gtype Character: [geomtype()] of the vector.
+#' @param cats Either `NULL` (default), or a numeric/integer vector with one unique value per point in `x`. Used to speed attachment of a database to a points vector, which is needed to extract coordinates using `v.to.db`. If `NULL`, category values are obtained by querying the points vector (which can be slow).
 #' 
 #' @noRd
-.crdsVect <- function(x, z, gtype = NULL) {
+.crdsVect <- function(x, z, gtype = NULL, cats = NULL) {
 
 	if (inherits(x, "GVector")) {
 		.locationRestore(x)
 		gtype <- geomtype(x)
 		src <- sources(x)
+		cats <- 1:nrow(x)
 	} else {
 		src <- x
 	}
@@ -91,7 +99,7 @@ st_coordinates <- function(x) {
 	# if lines or polygons, convert to points first
 	if (gtype %in% c("lines", "polygons")) {
 
-		stop("crds() will only work on points vectors.")
+		stop("crds() will only work on points vectors :(.")
 		
 		# ####### NB seems to work on lines but disagrees with st_coordinates() and crds()
 
@@ -105,7 +113,9 @@ st_coordinates <- function(x) {
 		# out <- terra::crds(pts)
 
 	} else if (gtype == "points") {
-		
+
+		if (!.vHasDatabase(src)) .vAttachDatabase(src, cats = cats)
+
 		info <- rgrass::execGRASS(
 			cmd = "v.to.db",
 			map = src,
@@ -114,7 +124,7 @@ st_coordinates <- function(x) {
 			type = "point",
 			intern = TRUE
 		)
-		
+
 		info <- info[!grepl(info, pattern = "cat|x|y|z")]
 		info <- info[grepl(info, pattern = "\\|")]
 		# cutAt <- which(info == "Reading features...")
