e17b385541b2e8a9ea98a20d7c181a493d29a7ca
Adam B. Smith <adam.smith@mobot.org>
2025-05-21 22:37:06 -0500

    Allow calculation in user-defined window w/ addon

--- R/ruggedness.r
+++ R/ruggedness.r
@@ -1,8 +1,12 @@
 #' Terrain ruggedness index
 #'
-#' @description For a given focal grid cell, the terrain ruggedness index (TRI) is calculated by taking the square root of the average of the squared difference between the focal cell's elevation and the elevations of the 8 surrounding cells, or \deqn{\sqrt(\sum_{i = 1}^{8}(m_i - m_0)^2 / 8)} where \eqn{m_0} is the elevation of the focal cell and \eqn{m_i} is the elevation of the *i*th grid cell.
+#' @description For a given focal grid cell, the terrain ruggedness index (TRI) is calculated by taking the square root of the average of the squared difference between the focal cell's elevation and the elevations of the 8 surrounding cells, or \deqn{\sqrt(\sum_{i = 1}^{8}(m_i - m_0)^2 / 8)} where \eqn{m_0} is the elevation of the focal cell and \eqn{m_i} is the elevation of the *i*th grid cell. Areas that are entirely flat will have a value of `NA` assigned to them.
+#'
+#' By default, TRI is calculated for a 3x3 moving window around each focal cell. However, you can use a larger-sized window. In this case, the window size must be an odd number >= 3, and you must have the `r.tri` **GRASS** addon installed. If it is not installed, the function will try to install it.
 #'
 #' @param x A `GRaster`.
+#' @param size Integer (default is 3): Size of the moving window. Must be an odd integer >= 3.
+#' @param exponent Numeric >= 0 and <= 4. Used to reduce the influence of cells farther from the focal cell (larger areas can yield noisier results if the exponent small). All cells are weighted equally when `exponent = 0`.
 #'
 #' @returns A `GRaster`.
 #'
@@ -18,7 +22,11 @@
 methods::setMethod(
 	f = "ruggedness",
 	signature = c(x = "GRaster"),
-	function(x) {
+	function(x, size = 3, exponent = 0) {
+
+	if (size %% 2 != 1) stop("Argument `size` must be an odd integer >= 3.")
+	if (exponent < 0 | exponent > 4) stop("Argument `exponent` must in the range [0, 4].")
+	if (size > 3) .addons("r.tri")
 
 	.locationRestore(x)
 	.region(x)
@@ -27,17 +35,33 @@ methods::setMethod(
 	srcs <- .makeSourceName("terrainRuggednessIndex_r_mapcalc", "raster", n = nLayers)
 	for (i in seq_len(nLayers)) {
 	
-		ex <- paste0(srcs[i], " = sqrt((",
-			"(", sources(x)[i], "[-1, -1] - ", sources(x), ")^2 + ",
-			"(", sources(x)[i], "[-1, 0] - ", sources(x), ")^2 + ",
-			"(", sources(x)[i], "[-1, 1] - ", sources(x), ")^2 + ",
-			"(", sources(x)[i], "[0, -1] - ", sources(x), ")^2 + ",
-			"(", sources(x)[i], "[0, 1] - ", sources(x), ")^2 + ",
-			"(", sources(x)[i], "[1, -1] - ", sources(x), ")^2 + ",
-			"(", sources(x)[i], "[1, 0] - ", sources(x), ")^2 + ",
-			"(", sources(x)[i], "[1, 1] - ", sources(x), ")^2) / 8)")
+		if (size == 3) {
+
+			ex <- paste0(srcs[i], " = sqrt((",
+				"(", sources(x)[i], "[-1, -1] - ", sources(x), ")^2 + ",
+				"(", sources(x)[i], "[-1, 0] - ", sources(x), ")^2 + ",
+				"(", sources(x)[i], "[-1, 1] - ", sources(x), ")^2 + ",
+				"(", sources(x)[i], "[0, -1] - ", sources(x), ")^2 + ",
+				"(", sources(x)[i], "[0, 1] - ", sources(x), ")^2 + ",
+				"(", sources(x)[i], "[1, -1] - ", sources(x), ")^2 + ",
+				"(", sources(x)[i], "[1, 0] - ", sources(x), ")^2 + ",
+				"(", sources(x)[i], "[1, 1] - ", sources(x), ")^2) / 8)")
+			
+			rgrass::execGRASS("r.mapcalc", expression = ex, flags = c(.quiet(), "overwrite"))
+
+		} else {
 		
-		rgrass::execGRASS("r.mapcalc", expression = ex, flags = c(.quiet(), "overwrite"))
+			rgrass::execGRASS(
+				cmd = "r.tri",
+				input = sources(x)[i],
+				output = srcs[i],
+				size = size,
+				exponent = exponent,
+				# processes = faster("cores"), # does not create raster
+				flags = c(.quiet(), "overwrite")
+			)
+		
+		}
 
 	} # next layer
 	.makeGRaster(srcs, names = paste0(names(x), "_TRI"))
