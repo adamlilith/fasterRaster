79ea086f3307fc59a710ecee7f8884d8bc7deac7
Adam B. Smith <adam.smith@mobot.org>
2025-10-17 22:26:14 -0500

    Update backdoor.r

--- R/backdoor.r
+++ R/backdoor.r
@@ -14,7 +14,7 @@
 	
 	faster(
 		grassDir = paste0("C:/Program Files/GRASS GIS ", verNice),
-		addonsDir = 'C:/Users/adame/AppData/Roaming/GRASS8/addons',
+		addonsDir = "C:/Users/adame/AppData/Roaming/GRASS8/addons",
 		memory = 1024 * 8,
 		cores = 2,
 		useDataTable = TRUE,
