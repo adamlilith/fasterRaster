0cc0881022fce1885562a50ee023b4b89431d50d
Adam B. Smith <adam.smith@mobot.org>
2026-08-27 23:43:13 -0500

    Update

--- man/examples/ex_asPoints.r
+++ man/examples/ex_asPoints.r
@@ -14,23 +14,22 @@ elev <- fast(madElev)
 coast <- fast(madCoast0)
 rivers <- fast(madRivers)
 
-# For this example, we will first crop to a small extent.
-river <- rivers[1]
-elevCrop <- crop(elev, river)
+# For speed, we will first crop the raster to a small extent:
+elevCrop <- crop(elev, c(2530000, 2532000, -1722000, -1720000))
 elevPoints <- as.points(elevCrop)
 elevPoints
 
 plot(elevCrop)
-plot(elevPoints, pch = '.', add = TRUE)
+plot(elevPoints, pch = 4, cex = 0.7, add = TRUE)
 
 # Extract points from vectors:
 coastPoints <- as.points(coast)
 riversPoints <- as.points(rivers)
 
 plot(coast)
-plot(coastPoints, add = TRUE)
+plot(coastPoints, pch = 4, cex = 0.7, add = TRUE)
 
 plot(rivers, col = "blue", add = TRUE)
-plot(riversPoints, col = "blue", add = TRUE)
+plot(riversPoints, col = "blue", pch = 1, cex = 0.7, add = TRUE)
 
 }
