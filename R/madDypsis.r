264bfe1b4515794c175542fdd628be4e03fb4d7e
Adam B. Smith <adam.smith@mobot.org>
2026-08-27 23:40:37 -0500

    Update with new GBIF download

--- R/madDypsis.r
+++ R/madDypsis.r
@@ -2,7 +2,7 @@
 #'
 #' @title Spatial points vector of records of Dypsis in eastern Madagascar
 #'
-#' @description Spatial points vector of herbarium specimens and observations of plants in the genus *Dypsis* (slender, evergreen palms) from a portion of eastern Madagascar.
+#' @description An `sf` points vector containing herbarium specimens and observations of plants in the genus *Dypsis* (slender, evergreen palms) from a portion of eastern Madagascar.
 #'
 #' @docType data
 #'
@@ -12,7 +12,7 @@
 #'
 #' @keywords Dypsis Madagascar
 #'
-#' @source [Global Biodiversity Information Facility (GBIF)](https://gbif.org)
+#' @source [Global Biodiversity Information Facility (GBIF)](https://gbif.org) Occurrence Data Download [https://doi.org/10.15468/dl.dq942k](https://doi.org/10.15468/dl.dq942k)
 #'
 #' @example man/examples/ex_fastData.r
 #'
