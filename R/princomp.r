024a927b067f06ab20009463813e501d5968d110
Adam B. Smith <adam.smith@mobot.org>
2026-09-16 23:29:33 -0500

    Update princomp.r

--- R/princomp.r
+++ R/princomp.r
@@ -125,7 +125,7 @@ methods::setMethod(
 #' 
 #' @returns An object of class `prcomp`.
 #' 
-#' @seealso [princomp()], [terra::princomp()], tool `i.pca` in **GRASS**
+#' @seealso [princomp()], [stats::princomp()], tool `i.pca` in **GRASS**
 #' 
 #' @example man/examples/ex_princomp.r
 #'
